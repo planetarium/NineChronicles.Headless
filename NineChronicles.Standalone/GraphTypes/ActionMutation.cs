a184bc33d68838283839dd143bc4bd6e0bd9d66c
Lee Suho <riemannulus@hitagi.moe>
2020-10-06 11:35:57 +0900

    Introduce HackAndSlash

--- NineChronicles.Standalone/GraphTypes/ActionMutation.cs
+++ NineChronicles.Standalone/GraphTypes/ActionMutation.cs
@@ -7,6 +7,7 @@ using Libplanet.Crypto;
 using Nekoyume.Action;
 using Serilog;
 using System;
+using System.Collections.Generic;
 using NineChroniclesActionType = Libplanet.Action.PolymorphicAction<Nekoyume.Action.ActionBase>;
 
 namespace NineChronicles.Standalone.GraphTypes
@@ -24,7 +25,7 @@ namespace NineChronicles.Standalone.GraphTypes
                         PrivateKey privatekey = service.PrivateKey;
                         BlockChain<NineChroniclesActionType> blockChain = service.Swarm.BlockChain;
                         Address userAddress = privatekey.PublicKey.ToAddress();
-                        Address avatarAddress = userAddress.Derive("avatar");
+                        Address avatarAddress = userAddress.Derive("avatar_0");
 
                         var action = new CreateAvatar
                         {
@@ -50,6 +51,55 @@ namespace NineChronicles.Standalone.GraphTypes
 
                     return true;
                 });
+
+                Field<NonNullGraphType<BooleanGraphType>>("hackAndSlash",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<StringGraphType>>
+                    {
+                        Name = "weeklyArenaAddress",
+                    },
+                    new QueryArgument<NonNullGraphType<StringGraphType>>
+                    {
+                        Name = "rankingArenaAddress",
+                    }),
+                resolve: context =>
+                {
+                    try
+                    {
+                        NineChroniclesNodeService service = context.Source;
+                        PrivateKey privatekey = service.PrivateKey;
+                        BlockChain<NineChroniclesActionType> blockChain = service.Swarm.BlockChain;
+                        Address userAddress = privatekey.PublicKey.ToAddress();
+                        Address avatarAddress = userAddress.Derive("avatar_0");
+                        Address weeklyArenaAddress = new Address(context.GetArgument<string>("weeklyArenaAddress"));
+                        Address rankingArenaAddress = new Address(context.GetArgument<string>("rankingArenaAddress"));
+
+                        var action = new HackAndSlash
+                        {
+                            avatarAddress = avatarAddress,
+                            worldId = 1,
+                            stageId = 1,
+                            WeeklyArenaAddress = weeklyArenaAddress,
+                            RankingMapAddress = rankingArenaAddress,
+                            costumes = new List<int>(),
+                            equipments = new List<Guid>(),
+                            foods = new List<Guid>(),
+                        };
+
+                        var actions = new PolymorphicAction<ActionBase>[] { action };
+                        blockChain.MakeTransaction(privatekey, actions);
+                    }
+                    catch(Exception e)
+                    {
+                        var msg = $"Unexpected exception occurred during {typeof(ActionMutation)}: {e}";
+                        context.Errors.Add(new ExecutionError(msg, e));
+                        Log.Error(msg, e);
+                        return false;
+                    }
+
+                    return true;
+                });
+
         }
     }
 }
