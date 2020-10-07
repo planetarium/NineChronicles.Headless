8316769c71eda9a154464dbd14441c128d77607d
Lee Suho <riemannulus@hitagi.moe>
2020-10-07 12:21:34 +0900

    Introduce buy action

--- NineChronicles.Standalone/GraphTypes/ActionMutation.cs
+++ NineChronicles.Standalone/GraphTypes/ActionMutation.cs
@@ -192,6 +192,55 @@ namespace NineChronicles.Standalone.GraphTypes
 
                     return true;
                 });
+
+            Field<NonNullGraphType<BooleanGraphType>>("buy",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<StringGraphType>>
+                    {
+                        Name = "sellerAgentAddress",
+                    },
+                    new QueryArgument<NonNullGraphType<StringGraphType>>
+                    {
+                        Name = "sellerAvatarAddress",
+                    },
+                    new QueryArgument<NonNullGraphType<StringGraphType>>
+                    {
+                        Name = "productId",
+                    }),
+                resolve: context =>
+                {
+                    try
+                    {
+                        NineChroniclesNodeService service = context.Source;
+                        PrivateKey privatekey = service.PrivateKey;
+                        BlockChain<NineChroniclesActionType> blockChain = service.Swarm.BlockChain;
+                        Address userAddress = privatekey.PublicKey.ToAddress();
+                        Address avatarAddress = userAddress.Derive("avatar_0");
+                        Address sellerAgentAddress = new Address(context.GetArgument<string>("sellerAgentAddress"));
+                        Address sellerAvatarAddress = new Address(context.GetArgument<string>("sellerAvatarAddress"));
+                        Guid productId = Guid.Parse(context.GetArgument<string>("productId"));
+
+                        var action = new Buy
+                        {
+                            buyerAvatarAddress = avatarAddress,
+                            sellerAgentAddress = sellerAgentAddress,
+                            sellerAvatarAddress = sellerAvatarAddress,
+                            productId = productId,
+                        };
+
+                        var actions = new PolymorphicAction<ActionBase>[] { action };
+                        blockChain.MakeTransaction(privatekey, actions);
+                    }
+                    catch (Exception e)
+                    {
+                        var msg = $"Unexpected exception occurred during {typeof(ActionMutation)}: {e}";
+                        context.Errors.Add(new ExecutionError(msg, e));
+                        Log.Error(msg, e);
+                        return false;
+                    }
+
+                    return true;
+                });
         }
     }
 }
