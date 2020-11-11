4f9fb6a230ff7755e4ef15b82d0bcc026f6af0f6
Lee, Suho <riemannulus@hitagi.moe>
2020-11-11 18:26:28 +0900

    feat: Introduce NodeStatusRenderer

---
+++ NineChronicles.Standalone/NodeStatusRenderer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using NineChronicles.RPC.Shared.Exceptions;
+
+namespace NineChronicles.Standalone
+{
+    public class NodeStatusRenderer
+    {
+        public readonly Subject<bool> PreloadStatusSubject = new Subject<bool>();
+
+        public void PreloadStatus(bool isPreloadEnded)
+        {
+            PreloadStatusSubject.OnNext(isPreloadEnded);
+        }
+
+        public IObservable<bool> EveryChangedStatus() => PreloadStatusSubject.AsObservable();
+    }
+}
