a3f69cc7596c9dfee14f76ec89b5d6b04325e338
Lee Dogeon <dev.moreal@gmail.com>
2021-02-17 21:53:18 +0900

    feat: introduce `--no-cors` option

--- NineChronicles.Headless/Properties/GraphQLNodeServiceProperties.cs
+++ NineChronicles.Headless/Properties/GraphQLNodeServiceProperties.cs
@@ -9,5 +9,7 @@ namespace NineChronicles.Headless.Properties
         public int? GraphQLListenPort { get; set; }
         
         public string SecretToken { get; set; }
+
+        public bool NoCors { get; set; }
     }
 }
