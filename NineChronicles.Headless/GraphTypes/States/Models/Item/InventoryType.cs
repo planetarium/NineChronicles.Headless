2b326c102ef35503cb1113cea73399a44b2004d8
Yang Chun Ung <qooraven@gmail.com>
2021-02-23 18:49:30 +0900

    Add inventory description

--- NineChronicles.Headless/GraphTypes/States/Models/Item/InventoryType.cs
+++ NineChronicles.Headless/GraphTypes/States/Models/Item/InventoryType.cs
@@ -7,10 +7,22 @@ namespace NineChronicles.Headless.GraphTypes.States.Models.Item
     {
         public InventoryType()
         {
-            Field<NonNullGraphType<ListGraphType<NonNullGraphType<ConsumableType>>>>(nameof(Inventory.Consumables));
-            Field<NonNullGraphType<ListGraphType<NonNullGraphType<MaterialType>>>>(nameof(Inventory.Materials));
-            Field<NonNullGraphType<ListGraphType<NonNullGraphType<EquipmentType>>>>(nameof(Inventory.Equipments));
-            Field<NonNullGraphType<ListGraphType<NonNullGraphType<CostumeType>>>>(nameof(Inventory.Costumes));
+            Field<NonNullGraphType<ListGraphType<NonNullGraphType<ConsumableType>>>>(
+                nameof(Inventory.Consumables),
+                description: "List of Consumable."
+            );
+            Field<NonNullGraphType<ListGraphType<NonNullGraphType<MaterialType>>>>(
+                nameof(Inventory.Materials),
+                description: "List of Material."
+            );
+            Field<NonNullGraphType<ListGraphType<NonNullGraphType<EquipmentType>>>>(
+                nameof(Inventory.Equipments),
+                description: "List of Equipment."
+            );
+            Field<NonNullGraphType<ListGraphType<NonNullGraphType<CostumeType>>>>(
+                nameof(Inventory.Costumes),
+                description: "List of Costumez."
+            );
         }
     }
 }
