dd621a5c83342e092a10489fe535bf4e0cb2f2f6
Yang Chun Ung <qooraven@gmail.com>
2021-02-23 18:29:25 +0900

    Add RankingMapState description

--- NineChronicles.Headless/GraphTypes/States/RankingInfoType.cs
+++ NineChronicles.Headless/GraphTypes/States/RankingInfoType.cs
@@ -9,27 +9,35 @@ namespace NineChronicles.Headless.GraphTypes.States
         {
             Field<NonNullGraphType<LongGraphType>>(
                 nameof(RankingInfo.Exp),
+                description: "AvatarState total EXP.",
                 resolve: context => context.Source.Exp);
             Field<NonNullGraphType<IntGraphType>>(
                 nameof(RankingInfo.Level),
+                description: "AvatarState Level.",
                 resolve: context => context.Source.Level);
             Field<NonNullGraphType<IntGraphType>>(
                 nameof(RankingInfo.ArmorId),
+                description: "Equipped Armor id from EquipmentItemSheet.",
                 resolve: context => context.Source.ArmorId);
             Field<NonNullGraphType<LongGraphType>>(
                 nameof(RankingInfo.UpdatedAt),
+                description: "RankingInfo updated block index.",
                 resolve: context => context.Source.UpdatedAt);
             Field<NonNullGraphType<LongGraphType>>(
                 nameof(RankingInfo.StageClearedBlockIndex),
+                description: "Latest stage cleared block index.",
                 resolve: context => context.Source.StageClearedBlockIndex);
             Field<NonNullGraphType<AddressType>>(
                 nameof(RankingInfo.AgentAddress),
+                description: "Address of AgentState.",
                 resolve: context => context.Source.AgentAddress);
             Field<NonNullGraphType<AddressType>>(
                 nameof(RankingInfo.AvatarAddress),
+                description: "Address of AvatarState.",
                 resolve: context => context.Source.AvatarAddress);
             Field<NonNullGraphType<StringGraphType>>(
                 nameof(RankingInfo.AvatarName),
+                description: "AvatarState name.",
                 resolve: context => context.Source.AvatarName);
         }
     }
