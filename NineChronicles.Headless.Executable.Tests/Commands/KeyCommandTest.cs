e8e46ce1b6eb7c82dfcca110b8e0949b029eb20f
Moreal <dev.moreal@gmail.com>
2021-03-15 10:46:59 +0900

    test: complement unittest of `KeyCommand.Remove`

---
+++ NineChronicles.Headless.Executable.Tests/Commands/KeyCommandTest.cs
@@ -0,0 +1,34 @@
+using System;
+using Libplanet.Crypto;
+using Libplanet.Extensions.Cocona.Commands;
+using Libplanet.KeyStore;
+using NineChronicles.Headless.Executable.Tests.KeyStore;
+using Xunit;
+
+namespace NineChronicles.Headless.Executable.Tests.Commands
+{
+    public class KeyCommandTest
+    {
+        private readonly IKeyStore _keyStore;
+        private readonly KeyCommand _keyCommand;
+
+        public KeyCommandTest()
+        {
+            _keyStore = new InMemoryKeyStore();
+            _keyCommand = new KeyCommand(_keyStore);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("foo", "bar")]
+        public void Remove_ByForce(string passphrase, string inputPassphrase)
+        {
+            PrivateKey privateKey = new PrivateKey();
+            Guid keyId = _keyStore.Add(ProtectedPrivateKey.Protect(privateKey, passphrase));
+            
+            Assert.Contains(keyId, _keyStore.ListIds());
+            _keyCommand.Remove(keyId, passphrase: inputPassphrase, force: true);
+            Assert.DoesNotContain(keyId, _keyStore.ListIds());
+        }
+    }
+}
