e300b4879cad1d30738bd5a6f2e3f1da515ddda3
Moreal <dev.moreal@gmail.com>
2021-03-16 11:08:45 +0900

    feat(cli): introduce conversion subcommand

---
+++ NineChronicles.Headless.Executable/Commands/ConversionCommand.cs
@@ -0,0 +1,37 @@
+using System;
+using Cocona;
+using Libplanet;
+using Libplanet.Crypto;
+using NineChronicles.Headless.Executable.IO;
+
+namespace NineChronicles.Headless.Executable.Commands
+{
+    public class ConversionCommand
+    {
+        private readonly IConsole _console;
+
+        public ConversionCommand(IConsole console)
+        {
+            _console = console;
+        }
+
+        public enum ConversionTarget
+        {
+            Address,
+            PublicKey,
+        }
+
+        public void PrivateKey([Argument("PRIVATE-KEY")]string privateKeyHex, [Argument("TARGET-TYPE")] ConversionTarget to)
+        {
+            var privateKey = new PrivateKey(ByteUtil.ParseHex(privateKeyHex));
+            string output = to switch
+            {
+                ConversionTarget.Address => privateKey.ToAddress().ToHex(),
+                ConversionTarget.PublicKey => ByteUtil.Hex(privateKey.PublicKey.Format(true)),
+                _ => throw new ArgumentOutOfRangeException(nameof(to)),
+            };
+
+            _console.Out.WriteLine(output);
+        }
+    }
+}
