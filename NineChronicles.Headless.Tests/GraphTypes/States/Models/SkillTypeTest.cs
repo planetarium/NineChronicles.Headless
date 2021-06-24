3a5f1bb2a4b7106412bf4a5ffba38fb6257ac1dd
Yang Chun Ung <qooraven@gmail.com>
2021-06-24 22:48:01 +0900

    Add more equipment information

---
+++ NineChronicles.Headless.Tests/GraphTypes/States/Models/SkillTypeTest.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nekoyume.Model.Skill;
+using Xunit;
+using static NineChronicles.Headless.Tests.GraphQLTestUtils;
+
+namespace NineChronicles.Headless.Tests.GraphTypes.States.Models
+{
+    public class SkillTypeTest
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(20, 300)]
+        public async Task Query(int power, int chance)
+        {
+            const string query = @"
+            {
+                id
+                elementalType
+                power
+                chance
+            }";
+
+            var row = Fixtures.TableSheetsFX.SkillSheet.OrderedList.First();
+            var skill = SkillFactory.Get(row, power, chance);
+            var queryResult = await ExecuteQueryAsync<Headless.GraphTypes.States.Models.SkillType>(query, source: skill);
+            Assert.Equal(new Dictionary<string, object>
+            {
+                ["id"] = row.Id,
+                ["elementalType"] = row.ElementalType.ToString().ToUpper(),
+                ["power"] = power,
+                ["chance"] = chance,
+            }, queryResult.Data);
+
+        }
+    }
+}
---
+++ NineChronicles.Headless/GraphTypes/States/Models/SkillType.cs
@@ -0,0 +1,21 @@
+using GraphQL.Types;
+using Nekoyume.Model.Skill;
+using NineChronicles.Headless.GraphTypes.States.Models.Item.Enum;
+
+namespace NineChronicles.Headless.GraphTypes.States.Models
+{
+    public class SkillType : ObjectGraphType<Skill>
+    {
+        public SkillType()
+        {
+            Field<NonNullGraphType<IntGraphType>>(
+                "id",
+                resolve: context => context.Source.SkillRow.Id);
+            Field<NonNullGraphType<ElementalTypeEnumType>>(
+                "elementalType",
+                resolve: context => context.Source.SkillRow.ElementalType);
+            Field<NonNullGraphType<IntGraphType>>(nameof(Skill.Power));
+            Field<NonNullGraphType<IntGraphType>>(nameof(Skill.Chance));
+        }
+    }
+}
