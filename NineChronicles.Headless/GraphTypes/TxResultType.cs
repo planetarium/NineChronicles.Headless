b7e020ff4e7f2850e5880a39645e63c0563b3e34
Lee Suho <riemannulus@hitagi.moe>
2021-08-02 18:25:04 +0900

    feat: transaction result

---
+++ NineChronicles.Headless/GraphTypes/TxResultType.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using GraphQL.Types;
+using Libplanet.Action;
+using Libplanet.Blocks;
+using Libplanet.Tx;
+
+namespace NineChronicles.Headless.GraphTypes
+{
+    public class TxResultType : ObjectGraphType<TxResult>
+    {
+        public TxResultType()
+        {
+            Field<NonNullGraphType<TxStatusType>>(
+                nameof(TxResult.TxStatus),
+                description: "The transaction status.",
+                resolve: context => context.Source.TxStatus
+            );
+
+            Field<LongGraphType>(
+                nameof(TxResult.BlockIndex),
+                description: "The block index which the target transaction executed.",
+                resolve: context => context.Source.BlockIndex
+            );
+
+            Field<StringGraphType>(
+                nameof(TxResult.BlockHash),
+                description: "The block hash which the target transaction executed.",
+                resolve: context => context.Source.BlockHash
+            );
+        }
+    }
+}
