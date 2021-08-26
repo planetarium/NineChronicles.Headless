0316bf463127e9e9d6164d868491d3021269b440
Yang Chun Ung <qooraven@gmail.com>
2021-08-26 11:19:39 +0900

    Add addressActivated query

--- NineChronicles.Headless/GraphTypes/ActivationStatusQuery.cs
+++ NineChronicles.Headless/GraphTypes/ActivationStatusQuery.cs
@@ -6,6 +6,7 @@ using Nekoyume.Action;
 using Nekoyume.Model;
 using Nekoyume.Model.State;
 using System;
+using Libplanet.Explorer.GraphTypes;
 using Log = Serilog.Log;
 
 namespace NineChronicles.Headless.GraphTypes
@@ -38,6 +39,62 @@ namespace NineChronicles.Headless.GraphTypes
                         }
 
                         Address userAddress = privateKey.ToAddress();
+                        Address activatedAddress = userAddress.Derive(ActivationKey.DeriveKey);
+
+                        if (blockChain.GetState(activatedAddress) is Bencodex.Types.Boolean)
+                        {
+                            return true;
+                        }
+
+                        // Preserve previous check code due to migration period.
+                        // TODO: Remove this code after v100061+
+                        IValue state = blockChain.GetState(ActivatedAccountsState.Address);
+
+                        if (state is Bencodex.Types.Dictionary asDict)
+                        {
+                            var activatedAccountsState = new ActivatedAccountsState(asDict);
+                            var activatedAccounts = activatedAccountsState.Accounts;
+                            return activatedAccounts.Count == 0
+                                   || activatedAccounts.Contains(userAddress);
+                        }
+
+                        return true;
+                    }
+                    catch (Exception e)
+                    {
+                        var msg = "Unexpected exception occurred during ActivationStatusQuery: {e}";
+                        context.Errors.Add(new ExecutionError(msg, e));
+                        Log.Error(msg, e);
+                        return false;
+                    }
+                }
+            );
+
+            Field<NonNullGraphType<BooleanGraphType>>(
+                name: "addressActivated",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<AddressType>>
+                    {
+                        Name = "addressHex"
+                    }
+                ),
+                resolve: context =>
+                {
+                    var service = standaloneContext.NineChroniclesNodeService;
+
+                    if (service is null)
+                    {
+                        return false;
+                    }
+
+                    try
+                    {
+                        if (!(service.Swarm?.BlockChain is { } blockChain))
+                        {
+                            throw new InvalidOperationException($"{nameof(service.Swarm.BlockChain)} is null.");
+                        }
+
+                        var userAddress = context.GetArgument<Address>("addressHex");
                         Address activatedAddress = userAddress.Derive(ActivationKey.DeriveKey);
 
                         if (blockChain.GetState(activatedAddress) is Bencodex.Types.Boolean)
