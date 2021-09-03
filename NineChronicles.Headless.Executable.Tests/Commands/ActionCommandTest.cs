7b4e00cfe6351d6784a491f0b87c8924d5af0677
Yang Chun Ung <qooraven@gmail.com>
2021-09-03 14:40:30 +0900

    Add action command

---
+++ NineChronicles.Headless.Executable.Tests/Commands/ActionCommandTest.cs
@@ -0,0 +1,56 @@
+using System.IO;
+using System.Net;
+using Bencodex;
+using Bencodex.Types;
+using Libplanet;
+using Libplanet.Blocks;
+using Libplanet.Crypto;
+using Libplanet.Tx;
+using Nekoyume.Action;
+using Nekoyume.Model;
+using Nekoyume.Model.State;
+using NineChronicles.Headless.Executable.Commands;
+using NineChronicles.Headless.Executable.IO;
+using NineChronicles.Headless.Executable.Tests.IO;
+using Xunit;
+using NCAction = Libplanet.Action.PolymorphicAction<Nekoyume.Action.ActionBase>;
+
+
+namespace NineChronicles.Headless.Executable.Tests.Commands
+{
+    public class ActionCommandTest
+    {
+        private readonly StringIOConsole _console;
+        private readonly ActionCommand _command;
+        private readonly Codec _codec = new Codec();
+
+        public ActionCommandTest()
+        {
+            _console = new StringIOConsole();
+            _command = new ActionCommand(_console);
+        }
+
+        [Fact]
+        public void ActivateAccount()
+        {
+            var nonce = new byte[] { 0x00, 0x01, 0x02, 0x03 };
+            var privateKey = new PrivateKey();
+            (ActivationKey activationKey, PendingActivationState _) =
+                ActivationKey.Create(privateKey, nonce);
+            var filePath = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
+            var resultCode = _command.ActivateAccount(activationKey.Encode(), ByteUtil.Hex(nonce), filePath);
+            Assert.Equal(0, resultCode);
+
+            var rawAction = ByteUtil.ParseHex(File.ReadAllText(filePath));
+            var decoded = (List)_codec.Decode(rawAction);
+            string type = (Text)decoded[0];
+            Assert.Equal(nameof(Nekoyume.Action.ActivateAccount), type);
+
+            Dictionary plainValue = (Dictionary)decoded[1];
+            var action = new ActivateAccount();
+            action.LoadPlainValue(plainValue);
+            Assert.Equal(activationKey.PrivateKey.Sign(nonce), action.Signature);
+            Assert.Equal(activationKey.PendingAddress, action.PendingAddress);
+        }
+    }
+}
