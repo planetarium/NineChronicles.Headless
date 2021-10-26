399d82262de5e0b3b2ca6f2534cb69721f6af83c
area363 <kidonseo@gmail.com>
2021-10-26 14:02:11 +0900

    add TransferAsset action command

--- NineChronicles.Headless.Executable.Tests/Commands/ActionCommandTest.cs
+++ NineChronicles.Headless.Executable.Tests/Commands/ActionCommandTest.cs
@@ -1,14 +1,24 @@
 using System;
 using System.IO;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 using Bencodex;
 using Bencodex.Types;
 using Libplanet;
+using Libplanet.Assets;
+using Libplanet.Blockchain;
+using Libplanet.Blockchain.Policies;
 using Libplanet.Crypto;
+using Libplanet.Store;
+using Libplanet.Store.Trie;
 using Nekoyume.Action;
+using Nekoyume.BlockChain.Policy;
 using Nekoyume.Model;
 using Nekoyume.Model.State;
 using NineChronicles.Headless.Executable.Commands;
+using NineChronicles.Headless.Executable.Store;
 using NineChronicles.Headless.Executable.Tests.IO;
+using Serilog.Core;
 using Xunit;
 using NCAction = Libplanet.Action.PolymorphicAction<Nekoyume.Action.ActionBase>;
 
@@ -20,11 +30,13 @@ namespace NineChronicles.Headless.Executable.Tests.Commands
         private readonly StringIOConsole _console;
         private readonly ActionCommand _command;
         private readonly Codec _codec = new Codec();
+        private readonly string _storePath;
 
         public ActionCommandTest()
         {
             _console = new StringIOConsole();
             _command = new ActionCommand(_console);
+            _storePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
         }
 
         [Theory]
@@ -56,6 +68,70 @@ namespace NineChronicles.Headless.Executable.Tests.Commands
             else
             {
                 Assert.Contains("hexWithSlash seems invalid. [invalid_code]", _console.Error.ToString());
+            }
+        }
+
+        [Theory]
+        [InlineData(10, 0, "transfer asset test1.")]
+        [InlineData(100, 0, "transfer asset test2.")]
+        [InlineData(1000, 0, null)]
+        public async Task TransferAsset(
+            int amount,
+            int expectedCode,
+            string? memo = null)
+        {
+            var genesisBlock = BlockChain<NCAction>.MakeGenesisBlock(HashAlgorithmType.Of<SHA256>());
+            IStore store = StoreType.RocksDb.CreateStore(_storePath);
+            Guid chainId = Guid.NewGuid();
+            store.SetCanonicalChainId(chainId);
+            store.PutBlock(genesisBlock);
+            store.AppendIndex(chainId, genesisBlock.Hash);
+            var stateStore = new TrieStateStore(new DefaultKeyValueStore(null));
+            const int minimumDifficulty = 5000000, maximumTransactions = 100;
+            IStagePolicy<NCAction> stagePolicy = new VolatileStagePolicy<NCAction>();
+            IBlockPolicy<NCAction> blockPolicy = new BlockPolicySource(Logger.None).GetPolicy(
+                minimumDifficulty,
+                maximumTransactions);
+            BlockChain<NCAction> blockChain = new BlockChain<NCAction>(
+                blockPolicy,
+                stagePolicy,
+                store,
+                stateStore,
+                genesisBlock);
+            var senderPrivateKey = new PrivateKey();
+            var recipientPrivateKey = new PrivateKey();
+            await blockChain.MineBlock(senderPrivateKey);
+            store.Dispose();
+            stateStore.Dispose();
+            var filePath = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
+            var resultCode = _command.TransferAsset(
+                senderPrivateKey.ToAddress().ToHex(), 
+                recipientPrivateKey.ToAddress().ToHex(),
+                Convert.ToString(amount),
+                filePath,
+                memo);
+            Assert.Equal(expectedCode, resultCode);
+
+            if (resultCode == 0)
+            {
+                var rawAction = Convert.FromBase64String(File.ReadAllText(filePath));
+                var decoded = (List)_codec.Decode(rawAction);
+                string type = (Text)decoded[0];
+                Assert.Equal(nameof(Nekoyume.Action.TransferAsset), type);
+                var currency = new Currency("NCG", 2, minter: null);
+                FungibleAssetValue amountFungibleAssetValue =
+                    FungibleAssetValue.Parse(currency, Convert.ToString(amount));
+                Dictionary plainValue = (Dictionary)decoded[1];
+                var action = new TransferAsset();
+                action.LoadPlainValue(plainValue);
+                Assert.Equal(memo, action.Memo);
+                Assert.Equal(amountFungibleAssetValue, action.Amount);
+                Assert.Equal(senderPrivateKey.ToAddress(), action.Sender);
+                Assert.Equal(recipientPrivateKey.ToAddress(), action.Recipient);
+            }
+            else
+            {
+                Assert.Contains("System.FormatException: Could not find any recognizable digits.", _console.Error.ToString());
             }
         }
 
