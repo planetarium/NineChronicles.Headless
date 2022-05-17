47061aa218f0024b6f3f6728e19c95430d2a7a75
moreal <dev.moreal@gmail.com>
2022-05-17 21:28:00 +0900

    Add `deposit` field to `StakeState`

--- NineChronicles.Headless.Tests/GraphTypes/States/Models/StakeStateTypeTest.cs
+++ NineChronicles.Headless.Tests/GraphTypes/States/Models/StakeStateTypeTest.cs
@@ -1,6 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Bencodex.Types;
 using GraphQL.Execution;
+using Libplanet;
+using Libplanet.Assets;
 using Nekoyume.Model.State;
 using NineChronicles.Headless.GraphTypes.States;
 using Xunit;
@@ -12,17 +16,43 @@ namespace NineChronicles.Headless.Tests.GraphTypes.States.Models
     {
         [Theory]
         [MemberData(nameof(Members))]
-        public async Task Query(StakeState stakeState, Dictionary<string, object> expected)
+        public async Task Query(StakeState stakeState, long deposit, Dictionary<string, object> expected)
         {
+            var goldCurrency = new Currency("NCG", 2, minter: null);
+
+            IValue? GetStateMock(Address address)
+            {
+                if (GoldCurrencyState.Address == address)
+                {
+                    return new GoldCurrencyState(goldCurrency).Serialize();
+                }
+
+                return null;
+            }
+
+            IReadOnlyList<IValue?> GetStatesMock(IReadOnlyList<Address> addresses) =>
+                addresses.Select(GetStateMock).ToArray();
+
+            FungibleAssetValue GetBalanceMock(Address address, Currency currency)
+            {
+                if (address == Fixtures.StakeStateAddress)
+                {
+                    return goldCurrency * deposit;
+                }
+
+                return FungibleAssetValue.FromRawValue(currency, 0);
+            }
+
             const string query = @"
             {
                 address
+                deposit
                 startedBlockIndex
                 receivedBlockIndex
                 cancellableBlockIndex
                 claimableBlockIndex
             }";
-            var queryResult = await ExecuteQueryAsync<StakeStateType>(query, source: stakeState);
+            var queryResult = await ExecuteQueryAsync<StakeStateType>(query, source: new StakeStateType.StakeStateContext(stakeState, GetStatesMock, GetBalanceMock));
             var data = (Dictionary<string, object>)((ExecutionNode)queryResult.Data!).ToValue()!;
             Assert.Equal(expected, data);
         }
@@ -32,9 +62,11 @@ namespace NineChronicles.Headless.Tests.GraphTypes.States.Models
             new object[]
             {
                 new StakeState(Fixtures.StakeStateAddress, 0),
+                100,
                 new Dictionary<string, object>
                 {
                     ["address"] = Fixtures.StakeStateAddress.ToString(),
+                    ["deposit"] = "100.00",
                     ["startedBlockIndex"] = 0,
                     ["cancellableBlockIndex"] = StakeState.LockupInterval,
                     ["receivedBlockIndex"] = 0,
--- NineChronicles.Headless/GraphTypes/States/StakeStateType.cs
+++ NineChronicles.Headless/GraphTypes/States/StakeStateType.cs
@@ -1,4 +1,6 @@
+using Bencodex.Types;
 using GraphQL.Types;
+using Libplanet.Action;
 using Libplanet.Explorer.GraphTypes;
 using Nekoyume.Model.State;
 using NineChronicles.Headless.GraphTypes.States.Models;
@@ -9,34 +11,52 @@ using NineChronicles.Headless.GraphTypes.States.Models.Quest;
 
 namespace NineChronicles.Headless.GraphTypes.States
 {
-    public class StakeStateType : ObjectGraphType<StakeState>
+    public class StakeStateType : ObjectGraphType<StakeStateType.StakeStateContext>
     {
+        public class StakeStateContext : StateContext
+        {
+            public StakeStateContext(StakeState stakeState, AccountStateGetter accountStateGetter, AccountBalanceGetter accountBalanceGetter)
+                : base(accountStateGetter, accountBalanceGetter)
+            {
+                StakeState = stakeState;
+            }
+
+            public StakeState StakeState { get; }
+        }
+
         public StakeStateType()
         {
             Field<NonNullGraphType<AddressType>>(
                 nameof(StakeState.address),
                 description: "The address of current state.",
-                resolve: context => context.Source.address);
+                resolve: context => context.Source.StakeState.address);
+            Field<NonNullGraphType<StringGraphType>>(
+                "deposit",
+                description: "The staked amount.",
+                resolve: context => context.Source.AccountBalanceGetter(
+                        context.Source.StakeState.address,
+                        new GoldCurrencyState((Dictionary)context.Source.GetState(GoldCurrencyState.Address)!).Currency)
+                    .GetQuantityString(true));
             Field<NonNullGraphType<IntGraphType>>(
                 nameof(StakeState.StartedBlockIndex),
                 description: "The block index the user started to stake.",
-                resolve: context => context.Source.StartedBlockIndex);
+                resolve: context => context.Source.StakeState.StartedBlockIndex);
             Field<NonNullGraphType<IntGraphType>>(
                 nameof(StakeState.ReceivedBlockIndex),
                 description: "The block index the user received rewards.",
-                resolve: context => context.Source.ReceivedBlockIndex);
+                resolve: context => context.Source.StakeState.ReceivedBlockIndex);
             Field<NonNullGraphType<LongGraphType>>(
                 nameof(StakeState.CancellableBlockIndex),
                 description: "The block index the user can cancel the staking.",
-                resolve: context => context.Source.CancellableBlockIndex);
+                resolve: context => context.Source.StakeState.CancellableBlockIndex);
             Field<NonNullGraphType<LongGraphType>>(
                 "claimableBlockIndex",
                 description: "The block index the user can claim rewards.",
-                resolve: context => context.Source.ReceivedBlockIndex + StakeState.RewardInterval);
+                resolve: context => context.Source.StakeState.ReceivedBlockIndex + StakeState.RewardInterval);
             Field<NonNullGraphType<StakeAchievementsType>>(
                 nameof(StakeState.Achievements),
                 description: "The staking achievements.",
-                resolve: context => context.Source.Achievements);
+                resolve: context => context.Source.StakeState.Achievements);
         }
     }
 }
