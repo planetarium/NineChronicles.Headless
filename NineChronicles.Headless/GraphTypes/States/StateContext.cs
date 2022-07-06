2958d6753a2e51cddf55f797fb0a0e9ec30316c3
Troy Spjute <troy@prenticeworx.com>
2022-07-05 20:02:59 -0600

    Allow additional state extension methods
    Additional state extension methods are available if the context is also IAccountStateDelta

--- NineChronicles.Headless/GraphTypes/States/StateContext.cs
+++ NineChronicles.Headless/GraphTypes/States/StateContext.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using Bencodex.Types;
 using Libplanet;
 using Libplanet.Action;
@@ -6,7 +7,7 @@ using Libplanet.Assets;
 
 namespace NineChronicles.Headless.GraphTypes.States
 {
-    public class StateContext
+    public class StateContext : IAccountStateDelta
     {
         public StateContext(AccountStateGetter accountStateGetter, AccountBalanceGetter accountBalanceGetter)
         {
@@ -17,6 +18,12 @@ namespace NineChronicles.Headless.GraphTypes.States
         public AccountStateGetter AccountStateGetter { get; }
         public AccountBalanceGetter AccountBalanceGetter { get; }
 
+        public IImmutableSet<Address> UpdatedAddresses => throw new System.NotImplementedException();
+
+        public IImmutableSet<Address> StateUpdatedAddresses => throw new System.NotImplementedException();
+
+        public IImmutableDictionary<Address, IImmutableSet<Currency>> UpdatedFungibleAssets => throw new System.NotImplementedException();
+
         public IValue? GetState(Address address) =>
             AccountStateGetter(new[] { address })[0];
 
@@ -25,5 +32,25 @@ namespace NineChronicles.Headless.GraphTypes.States
 
         public FungibleAssetValue GetBalance(Address address, Currency currency) =>
             AccountBalanceGetter(address, currency);
+
+        public IAccountStateDelta SetState(Address address, IValue state)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IAccountStateDelta MintAsset(Address recipient, FungibleAssetValue value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IAccountStateDelta TransferAsset(Address sender, Address recipient, FungibleAssetValue value, bool allowNegativeBalance = false)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IAccountStateDelta BurnAsset(Address owner, FungibleAssetValue value)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
