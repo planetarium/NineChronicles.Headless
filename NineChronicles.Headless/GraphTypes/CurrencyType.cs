fa82526f9a102f521ae7bd1da1afe27ac838b63b
Yang Chun Ung <qooraven@gmail.com>
2022-08-03 18:05:08 +0900

    Introduce CurrencyType

---
+++ NineChronicles.Headless/GraphTypes/CurrencyType.cs
@@ -0,0 +1,25 @@
+using GraphQL.Types;
+using Libplanet.Assets;
+using Libplanet.Explorer.GraphTypes;
+
+namespace NineChronicles.Headless.GraphTypes
+{
+    public class CurrencyType : ObjectGraphType<Currency>
+    {
+        public CurrencyType()
+        {
+            Field<NonNullGraphType<StringGraphType>>(
+                nameof(Currency.Ticker),
+                resolve: context => context.Source.Ticker
+            );
+            Field<NonNullGraphType<ByteGraphType>>(
+                nameof(Currency.DecimalPlaces),
+                resolve: context => context.Source.DecimalPlaces
+            );
+            Field<ListGraphType<AddressType>>(
+                nameof(Currency.Minters),
+                resolve: context => context.Source.Minters
+            );
+        }
+    }
+}
