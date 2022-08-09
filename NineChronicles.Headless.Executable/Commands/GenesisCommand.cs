83344e404489eb074caf453727194c2bffb301f2
hyeon <hyeon@planetariumhq.com>
2022-08-09 10:34:54 +0900

    Update genesis command
    - Create new genesis config structure
    - Add simple config example to start

--- NineChronicles.Headless.Executable/Commands/GenesisCommand.cs
+++ NineChronicles.Headless.Executable/Commands/GenesisCommand.cs
@@ -1,32 +1,33 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
-using System.Reflection;
+using System.Numerics;
+using System.Security.AccessControl;
 using System.Text.Json;
-using System.Text.RegularExpressions;
 using Bencodex;
 using Bencodex.Types;
 using Cocona;
+using CsvHelper.Configuration.Attributes;
 using Libplanet;
 using Libplanet.Action;
 using Libplanet.Blocks;
 using Libplanet.Crypto;
+using Libplanet.Explorer.GraphTypes;
 using Libplanet.Extensions.Cocona;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Nekoyume;
 using Nekoyume.Action;
-using Nekoyume.Model;
 using Nekoyume.Model.State;
 using NineChronicles.Headless.Executable.IO;
+using Serilog;
 using Lib9cUtils = Lib9c.DevExtensions.Utils;
 
 namespace NineChronicles.Headless.Executable.Commands
 {
     public class GenesisCommand : CoconaLiteConsoleAppBase
     {
-        private static readonly Codec Codec = new Codec();
+        private const int DefaultCurrencyValue = 10000;
+        private static readonly Codec _codec = new Codec();
         private readonly IConsole _console;
 
         public GenesisCommand(IConsole console)
@@ -34,105 +35,153 @@ namespace NineChronicles.Headless.Executable.Commands
             _console = console;
         }
 
-        [Command(Description = "Mine a new genesis block.")]
-        public void Mine(
-            [Argument("CONFIG", Description = "JSON config path to mine genesis block.")]
-            string configPath
+        private void ProcessData(DataConfig config, out Dictionary<string, string> tableSheets)
+        {
+            if (string.IsNullOrEmpty(config.TablePath))
+            {
+                throw Utils.Error("TablePath is not set.");
+            }
+
+            tableSheets = Lib9cUtils.ImportSheets(config.TablePath);
+        }
+
+        private void ProcessCurrency(
+            CurrencyConfig? config,
+            out PrivateKey initialMinter,
+            out List<GoldDistribution> initialDepositList
         )
         {
+            if (config is null)
+            {
+                Log.Information("CurrencyConfig not provided. Skip setting...");
+                initialMinter = new PrivateKey();
+                initialDepositList = new List<GoldDistribution>
+                {
+                    new()
+                    {
+                        Address = initialMinter.ToAddress(), AmountPerBlock = DefaultCurrencyValue,
+                        StartBlock = 0, EndBlock = 0
+                    }
+                };
+                return;
+            }
+
+            if (string.IsNullOrEmpty(config.Value.InitialMinter))
+            {
+                Log.Information("Private Key not provided. Create random one...");
+                initialMinter = new PrivateKey();
+            }
+            else
+            {
+                initialMinter = new PrivateKey(config.Value.InitialMinter);
+            }
+
+            initialDepositList = new List<GoldDistribution>();
+            if (config.Value.InitialCurrencyDeposit.Count == 0)
+            {
+                Log.Information("Initial currency deposit list not provided. " +
+                                $"Give initial ${DefaultCurrencyValue} currency to InitialMinter");
+                initialDepositList.Add(new GoldDistribution
+                {
+                    Address = initialMinter.ToAddress(),
+                    AmountPerBlock = DefaultCurrencyValue,
+                    StartBlock = 0,
+                    EndBlock = 0
+                });
+            }
+            else
+            {
+                initialDepositList = config.Value.InitialCurrencyDeposit;
+            }
+        }
+
+        private void ProcessAdmin(AdminConfig? config, PrivateKey initialMinter, out AdminState adminState)
+        {
+            // FIXME: If the `adminState` is not required inside `MineGenesisBlock`,
+            //        this logic will be much lighter.
+            adminState = new AdminState(new PrivateKey().ToAddress(), 0);
+            if (config is null)
+            {
+                Log.Information("AdminConfig not provided. Skip admin setting...");
+                return;
+            }
+
+            if (config.Value.Activate)
+            {
+                if (string.IsNullOrEmpty(config.Value.Address))
+                {
+                    Log.Information("Admin address not provided. Give admin privilege to initialMinter");
+                    adminState = new AdminState(initialMinter.ToAddress(), config.Value.ValidUntil);
+                }
+            }
+            else
+            {
+                Log.Information("Inactivate Admin. Skip admin setting...");
+            }
+
+            Log.Information("Admin config done");
+        }
+
+        private void ProcessExtra(ExtraConfig? config,
+            out List<PendingActivationState> pendingActivationStates
+        )
+        {
+            pendingActivationStates = new List<PendingActivationState>();
+
+            if (config is null)
+            {
+                Log.Information("Extra config not provided");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(config.Value.PendingActivationStatePath))
+            {
+                string hex = File.ReadAllText(config.Value.PendingActivationStatePath).Trim();
+                List decoded = (List)_codec.Decode(ByteUtil.ParseHex(hex));
+                CreatePendingActivations action = new();
+                action.LoadPlainValue(decoded[1]);
+                pendingActivationStates = action.PendingActivations.Select(
+                    pa => new PendingActivationState(pa.Nonce, new PublicKey(pa.PublicKey))
+                ).ToList();
+            }
+        }
+
+        [Command(Description = "Mine a new genesis block")]
+        public void Mine(
+            [Argument("CONFIG", Description = "JSON config path to mine genesis block")]
+            string configPath)
+        {
+            var loggerConf = new LoggerConfiguration();
+            Log.Logger = loggerConf.CreateLogger();
+
             var options = new JsonSerializerOptions
             {
                 AllowTrailingCommas = true,
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
             };
-
             string json = File.ReadAllText(configPath);
             GenesisConfig genesisConfig = JsonSerializer.Deserialize<GenesisConfig>(json, options);
-            if (string.IsNullOrEmpty(genesisConfig.TablePath))
-            {
-                throw Utils.Error("TablePath is not set.");
-            }
-
-            Dictionary<string, string> tableSheets = Lib9cUtils.ImportSheets(genesisConfig.TablePath);
-
-            Lib9cUtils.CreateActivationKey(
-                out List<PendingActivationState> pendingActivationStates,
-                out List<ActivationKey> activationKeys,
-                genesisConfig.ActivationKeyCount);
-
-            if (string.IsNullOrEmpty(genesisConfig.PrivateKey))
-            {
-                throw Utils.Error("PrivateKey is not set");
-            }
-
-            if (string.IsNullOrEmpty(genesisConfig.GoldDistributionPath))
-            {
-                throw Utils.Error("GoldDistributionPath is not set");
-            }
-
-            if (string.IsNullOrEmpty(genesisConfig.AdminAddress))
-            {
-                throw Utils.Error($"{nameof(genesisConfig.AdminAddress)} is not set");
-            }
 
             try
             {
-                GoldDistribution[] goldDistributions = GoldDistribution
-                    .LoadInDescendingEndBlockOrder(genesisConfig.GoldDistributionPath);
+                ProcessData(genesisConfig.Data, out var tableSheets);
 
-                AdminState adminState =
-                    new AdminState(new Address(genesisConfig.AdminAddress), genesisConfig.AdminValidUntil);
+                ProcessCurrency(genesisConfig.Currency, out var initialMinter, out var initialDepositList);
 
-                AuthorizedMinersState? authorizedMinersState = null;
-                if (genesisConfig.AuthorizedMinerConfig.Miners is { } miners && miners.Any())
-                {
-                    authorizedMinersState = new AuthorizedMinersState(
-                        miners: genesisConfig.AuthorizedMinerConfig.Miners.Select(a => new Address(a)),
-                        interval: genesisConfig.AuthorizedMinerConfig.Interval,
-                        validUntil: genesisConfig.AuthorizedMinerConfig.ValidUntil
-                    );
-                }
+                ProcessAdmin(genesisConfig.Admin, initialMinter, out var adminState);
 
-                List<ActionBase>? actions = null;
-                if (!(genesisConfig.Actions is null))
-                {
-                    actions = genesisConfig.Actions.Select(a =>
-                    {
-                        if (File.Exists(a))
-                        {
-                            a = File.ReadAllText(a);
-                        }
+                ProcessExtra(genesisConfig.Extra, out List<PendingActivationState> pendingActivationStates);
 
-                        var decoded = (List)Codec.Decode(Convert.FromBase64String(a));
-                        string actionType = (Text)decoded[0];
-                        Dictionary plainValue = (Dictionary)decoded[1];
-                        Type type = typeof(ActionBase).Assembly
-                            .GetTypes()
-                            .First(type => type.Namespace is { } @namespace &&
-                                           @namespace.StartsWith($"{nameof(Nekoyume)}.{nameof(Nekoyume.Action)}") &&
-                                           !type.IsAbstract &&
-                                           typeof(ActionBase).IsAssignableFrom(type) &&
-                                           type.Name == actionType);
-                        ActionBase action = (ActionBase)Activator.CreateInstance(type)!;
-                        action.LoadPlainValue(plainValue);
-                        return action;
-                    }).ToList();
-                }
-
+                // Mine genesis block
                 Block<PolymorphicAction<ActionBase>> block = BlockHelper.MineGenesisBlock(
-                    tableSheets,
-                    goldDistributions,
-                    pendingActivationStates.ToArray(),
-                    adminState,
-                    authorizedMinersState,
-                    ImmutableHashSet<Address>.Empty,
-                    genesisConfig.ActivationKeyCount != 0,
-                    null,
-                    new PrivateKey(ByteUtil.ParseHex(genesisConfig.PrivateKey))
+                    tableSheets: tableSheets,
+                    goldDistributions: initialDepositList.ToArray(),
+                    pendingActivationStates: pendingActivationStates.ToArray(),
+                    adminState: adminState,
+                    privateKey: initialMinter
                 );
 
                 Lib9cUtils.ExportBlock(block, "genesis-block");
-                Lib9cUtils.ExportKeys(activationKeys, "keys");
             }
             catch (Exception e)
             {
@@ -142,25 +191,41 @@ namespace NineChronicles.Headless.Executable.Commands
 
 #pragma warning disable S3459
         [Serializable]
-        private struct AuthorizedMinerConfig
+        private struct DataConfig
         {
-            public long Interval { get; set; }
-            public long ValidUntil { get; set; }
-            public List<string> Miners { get; set; }
+            public string TablePath { get; set; }
         }
 
         [Serializable]
+        private struct CurrencyConfig
+        {
+            public string InitialMinter { get; set; }
+            public List<GoldDistribution> InitialCurrencyDeposit { get; set; }
+        }
+
+        [Serializable]
+        private struct AdminConfig
+        {
+            public bool Activate { get; set; }
+            public string Address { get; set; }
+            public long ValidUntil { get; set; }
+        }
+
+        [Serializable]
+        private struct ExtraConfig
+        {
+            public string? PendingActivationStatePath { get; set; }
+        }
+
+        // Config to mine new genesis block.
+        [Serializable]
         private struct GenesisConfig
         {
-            public string PrivateKey { get; set; }
-            public string TablePath { get; set; }
-            public string GoldDistributionPath { get; set; }
-            public uint ActivationKeyCount { get; set; }
-            public string AdminAddress { get; set; }
-            public long AdminValidUntil { get; set; }
-            public AuthorizedMinerConfig AuthorizedMinerConfig { get; set; }
-            public List<string> ActivatedAccounts { get; set; }
-            public List<string> Actions { get; set; }
+            public DataConfig Data { get; set; }
+            public CurrencyConfig? Currency { get; set; }
+            public AdminConfig? Admin { get; set; }
+
+            public ExtraConfig? Extra { get; set; }
         }
 #pragma warning restore S3459
     }
