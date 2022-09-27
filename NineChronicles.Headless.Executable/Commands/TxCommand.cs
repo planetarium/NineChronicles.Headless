fe3a53415593017e7994ce9f1d341c2975b27312
hyeon <hyeon@planetariumhq.com>
2022-09-27 16:34:16 +0900

    Add TxCommands from lib9c

--- NineChronicles.Headless.Executable/Commands/TxCommand.cs
+++ NineChronicles.Headless.Executable/Commands/TxCommand.cs
@@ -1,15 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using Bencodex;
 using Bencodex.Types;
 using Cocona;
+using CsvHelper;
 using Libplanet;
+using Libplanet.Assets;
 using Libplanet.Blocks;
 using Libplanet.Crypto;
 using Libplanet.Tx;
 using Nekoyume.Action;
+using Nekoyume.Model;
+using Nekoyume.Model.State;
+using Nekoyume.TableData;
 using NineChronicles.Headless.Executable.IO;
 using NCAction = Libplanet.Action.PolymorphicAction<Nekoyume.Action.ActionBase>;
 
@@ -17,7 +23,7 @@ namespace NineChronicles.Headless.Executable.Commands
 {
     public class TxCommand : CoconaLiteConsoleAppBase
     {
-        private static readonly Codec Codec = new Codec();
+        private static readonly Codec _codec = new Codec();
         private readonly IConsole _console;
 
         public TxCommand(IConsole console)
@@ -49,7 +55,7 @@ namespace NineChronicles.Headless.Executable.Commands
                     a = File.ReadAllText(a);
                 }
 
-                var decoded = (List)Codec.Decode(Convert.FromBase64String(a));
+                var decoded = (List)_codec.Decode(Convert.FromBase64String(a));
                 string type = (Text)decoded[0];
                 Dictionary plainValue = (Dictionary)decoded[1];
 
@@ -85,6 +91,229 @@ namespace NineChronicles.Headless.Executable.Commands
             else
             {
                 _console.Out.WriteLine(Convert.ToBase64String(raw));
+            }
+        }
+
+        public void TransferAsset(
+            [Argument("SENDER", Description = "An address of sender.")]
+            string sender,
+            [Argument("RECIPIENT", Description = "An address of recipient.")]
+            string recipient,
+            [Argument("AMOUNT", Description = "An amount of gold to transfer.")]
+            int goldAmount,
+            [Argument("GENESIS-BLOCK", Description = "A genesis block containing InitializeStates.")]
+            string genesisBlock
+        )
+        {
+            byte[] genesisBytes = File.ReadAllBytes(genesisBlock);
+            var genesisDict = (Bencodex.Types.Dictionary)_codec.Decode(genesisBytes);
+            IReadOnlyList<Transaction<NCAction>> genesisTxs =
+                BlockMarshaler.UnmarshalBlockTransactions<NCAction>(genesisDict);
+            var initStates = (InitializeStates)genesisTxs.Single().CustomActions!.Single().InnerAction;
+            Currency currency = new GoldCurrencyState(initStates.GoldCurrency).Currency;
+
+            var action = new TransferAsset(
+                new Address(sender),
+                new Address(recipient),
+                currency * goldAmount
+            );
+
+            var bencoded = new List(
+                (Text)nameof(TransferAsset),
+                action.PlainValue
+            );
+
+            byte[] raw = _codec.Encode(bencoded);
+            Console.Write(ByteUtil.Hex(raw));
+        }
+
+        [Command(Description = "Create PatchTable action and dump it.")]
+        public void PatchTable(
+            [Argument("TABLE-PATH", Description = "A table file path for patch.")]
+            string tablePath
+        )
+        {
+            var tableName = Path.GetFileName(tablePath);
+            if (tableName.EndsWith(".csv"))
+            {
+                tableName = tableName.Split(".csv")[0];
+            }
+
+            Console.Error.Write("----------------\n");
+            Console.Error.Write(tableName);
+            Console.Error.Write("\n----------------\n");
+            var tableCsv = File.ReadAllText(tablePath);
+            Console.Error.Write(tableCsv);
+
+            var type = typeof(ISheet).Assembly
+                .GetTypes()
+                .First(type => type.Namespace is { } @namespace &&
+                               @namespace.StartsWith($"{nameof(Nekoyume)}.{nameof(Nekoyume.TableData)}") &&
+                               !type.IsAbstract &&
+                               typeof(ISheet).IsAssignableFrom(type) &&
+                               type.Name == tableName);
+            var sheet = (ISheet)Activator.CreateInstance(type)!;
+            sheet.Set(tableCsv);
+
+            var action = new PatchTableSheet
+            {
+                TableName = tableName,
+                TableCsv = tableCsv
+            };
+
+            var bencoded = new List(
+                (Text)nameof(PatchTableSheet),
+                action.PlainValue
+            );
+
+            byte[] raw = _codec.Encode(bencoded);
+            Console.WriteLine(ByteUtil.Hex(raw));
+        }
+
+        [Command(Description = "Create MigrationLegacyShop action and dump it.")]
+        public void MigrationLegacyShop()
+        {
+            var action = new MigrationLegacyShop();
+
+            var bencoded = new List(
+                (Text)nameof(Nekoyume.Action.MigrationLegacyShop),
+                action.PlainValue
+            );
+
+            byte[] raw = _codec.Encode(bencoded);
+            Console.WriteLine(ByteUtil.Hex(raw));
+        }
+
+        [Command(Description = "Create MigrationActivatedAccountsState action and dump it.")]
+        public void MigrationActivatedAccountsState()
+        {
+            var action = new MigrationActivatedAccountsState();
+            var bencoded = new List(
+                (Text)nameof(Nekoyume.Action.MigrationActivatedAccountsState),
+                action.PlainValue
+            );
+
+            byte[] raw = _codec.Encode(bencoded);
+            Console.WriteLine(ByteUtil.Hex(raw));
+        }
+
+        [Command(Description = "Create MigrationAvatarState action and dump it.")]
+        public void MigrationAvatarState(
+            [Argument("directory-path", Description = "path of the directory contained hex-encoded avatar states.")]
+            string directoryPath,
+            [Argument("output-path", Description = "path of the output file dumped action.")]
+            string outputPath
+        )
+        {
+            var files = Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories);
+            var avatarStates = files.Select(a =>
+            {
+                var raw = File.ReadAllText(a);
+                return (Dictionary)_codec.Decode(ByteUtil.ParseHex(raw));
+            }).ToList();
+            var action = new MigrationAvatarState()
+            {
+                avatarStates = avatarStates
+            };
+
+            var encoded = new List(
+                (Text)nameof(Nekoyume.Action.MigrationAvatarState),
+                action.PlainValue
+            );
+
+            byte[] raw = _codec.Encode(encoded);
+            File.WriteAllText(outputPath, ByteUtil.Hex(raw));
+        }
+
+        [Command(Description = "Create AddRedeemCode action and dump it.")]
+        public void AddRedeemCode(
+            [Argument("TABLE-PATH", Description = "A table file path for RedeemCodeListSheet")]
+            string tablePath
+        )
+        {
+            var tableCsv = File.ReadAllText(tablePath);
+            var action = new AddRedeemCode
+            {
+                redeemCsv = tableCsv
+            };
+            var encoded = new List(
+                (Text)nameof(Nekoyume.Action.AddRedeemCode),
+                action.PlainValue
+            );
+            byte[] raw = _codec.Encode(encoded);
+            Console.WriteLine(ByteUtil.Hex(raw));
+        }
+
+        [Command(Description = "Create CreatePendingActivations action and dump it.")]
+        public void CreatePendingActivations(
+            [Argument("CSV-PATH", Description = "A csv file path for CreatePendingActivations")]
+            string csvPath
+        )
+        {
+            var RecordType = new
+            {
+                EncodedActivationKey = string.Empty,
+                NonceHex = string.Empty,
+            };
+            using var reader = new StreamReader(csvPath);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var activations =
+                csv.GetRecords(RecordType)
+                    .Select(r => new PendingActivationState(
+                        ByteUtil.ParseHex(r.NonceHex),
+                        ActivationKey.Decode(r.EncodedActivationKey).PrivateKey.PublicKey)
+                    )
+                    .ToList();
+            var action = new CreatePendingActivations(activations);
+            var encoded = new List(
+                new IValue[]
+                {
+                    (Text)nameof(Nekoyume.Action.CreatePendingActivations),
+                    action.PlainValue
+                }
+            );
+            byte[] raw = _codec.Encode(encoded);
+            Console.WriteLine(ByteUtil.Hex(raw));
+        }
+
+        [Command(Description = "Create RenewAdminState action and dump it.")]
+        public void RenewAdminState(
+            [Argument("NEW-VALID-UNTIL")] long newValidUntil
+        )
+        {
+            RenewAdminState action = new RenewAdminState(newValidUntil);
+            var encoded = new List(
+                (Text)nameof(Nekoyume.Action.RenewAdminState),
+                action.PlainValue
+            );
+            byte[] raw = _codec.Encode(encoded);
+            Console.WriteLine(ByteUtil.Hex(raw));
+        }
+
+        [Command(Description = "Create ActvationKey-nonce pairs and dump them as csv")]
+        public void CreateActivationKeys(
+            [Argument("COUNT", Description = "An amount of pairs")]
+            int count
+        )
+        {
+            var rng = new Random();
+            var nonce = new byte[4];
+            Console.WriteLine("EncodedActivationKey,NonceHex");
+            foreach (int i in Enumerable.Range(0, count))
+            {
+                PrivateKey key;
+                while (true)
+                {
+                    key = new PrivateKey();
+                    if (key.ToByteArray().Length == 32)
+                    {
+                        break;
+                    }
+                }
+
+                rng.NextBytes(nonce);
+                var (ak, _) = ActivationKey.Create(key, nonce);
+                Console.WriteLine($"{ak.Encode()},{ByteUtil.Hex(nonce)}");
             }
         }
     }
