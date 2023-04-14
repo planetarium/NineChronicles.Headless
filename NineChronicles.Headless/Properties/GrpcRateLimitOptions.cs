8443865a298a945a8b54e71f0e44cb1b7ae04677
area363 <kidonseo@gmail.com>
2023-04-14 15:12:17 +0900

    remove unused property

--- NineChronicles.Headless/Properties/GrpcRateLimitOptions.cs
+++ NineChronicles.Headless/Properties/GrpcRateLimitOptions.cs
@@ -2,7 +2,6 @@ namespace NineChronicles.Headless.Properties
 {
     public class GrpcRateLimitOptions
     {
-        public const string GrpcRateLimit = "GrpcRateLimit";
         public int PermitLimit { get; set; } = 1;
         public int Window { get; set; } = 5;
         public int QueueLimit { get; set; } = 0;
