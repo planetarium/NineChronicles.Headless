d15957d9efe9faccc2dd7be85171cae171909cc2
Hyun Seungmin <boscohyun@gmail.com>
2023-06-07 21:32:03 +0900

    Introduce SimplifyFungibleAssetValueInputType

---
+++ NineChronicles.Headless/GraphTypes/SimplifyFungibleAssetValueInputType.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Numerics;
+using GraphQL.Types;
+using Libplanet.Assets;
+
+namespace NineChronicles.Headless.GraphTypes
+{
+    public class SimplifyFungibleAssetValueInputType : InputObjectGraphType<FungibleAssetValue>
+    {
+        public SimplifyFungibleAssetValueInputType()
+        {
+            Name = "SimplifyFungibleAssetValueInput";
+            Field<NonNullGraphType<SimplifyCurrencyInputType>>(
+                name: "currency");
+            Field<NonNullGraphType<BigIntGraphType>>(
+                name: "majorUnit",
+                description: "Major unit of currency");
+            Field<NonNullGraphType<BigIntGraphType>>(
+                name: "minorUnit",
+                description: "Minor unit of currency");
+        }
+
+        public override object ParseDictionary(IDictionary<string, object?> value)
+        {
+            var currency = (Currency)value["currency"]!;
+            var majorUnit = (BigInteger)value["majorUnit"]!;
+            var minorUnit = (BigInteger)value["minorUnit"]!;
+            return new FungibleAssetValue(currency, majorUnit, minorUnit);
+        }
+    }
+}
