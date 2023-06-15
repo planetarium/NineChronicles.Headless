f23dcd0ab6ac7b4ddb824db25bfa8c65eefd0d0a
area363 <kidonseo@gmail.com>
2023-06-15 16:34:26 +0900

    add ip ban feature

--- NineChronicles.Headless/Middleware/CustomRateLimitMiddleware.cs
+++ NineChronicles.Headless/Middleware/CustomRateLimitMiddleware.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using AspNetCoreRateLimit;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
+using NineChronicles.Headless.Properties;
 using Serilog;
 using ILogger = Serilog.ILogger;
 
@@ -12,15 +13,17 @@ namespace NineChronicles.Headless.Middleware
     {
         private readonly ILogger _logger;
         private readonly IRateLimitConfiguration _config;
+        private readonly IOptions<CustomIpRateLimitOptions> _options;
 
         public CustomRateLimitMiddleware(RequestDelegate next,
             IProcessingStrategy processingStrategy,
-            IOptions<IpRateLimitOptions> options,
+            IOptions<CustomIpRateLimitOptions> options,
             IIpPolicyStore policyStore,
             IRateLimitConfiguration config)
             : base(next, options?.Value, new CustomIpRateLimitProcessor(options?.Value!, policyStore, processingStrategy), config)
         {
             _config = config;
+            _options = options!;
             _logger = Log.Logger.ForContext<CustomRateLimitMiddleware>();
         }
 
@@ -29,6 +32,11 @@ namespace NineChronicles.Headless.Middleware
             _logger.Information($"[IP-RATE-LIMITER] Request {identity.HttpVerb}:{identity.Path} from IP {identity.ClientIp} has been blocked, " +
                                 $"quota {rule.Limit}/{rule.Period} exceeded by {counter.Count - rule.Limit}. Blocked by rule {rule.Endpoint}, " +
                                 $"TraceIdentifier {httpContext.TraceIdentifier}. MonitorMode: {rule.MonitorMode}");
+            if (counter.Count - rule.Limit >= _options.Value.IpBanThresholdCount)
+            {
+                _logger.Information($"[IP-RATE-LIMITER] Banning IP {identity.ClientIp}.");
+                IpBanMiddleware.BanIp(identity.ClientIp);
+            }
         }
 
         public override async Task<ClientRequestIdentity> ResolveIdentityAsync(HttpContext httpContext)
