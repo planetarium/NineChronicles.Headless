a08c03f78ee64ee2d005b2a81b7a60a2067dda1a
Hyun Seungmin <boscohyun@gmail.com>
2023-06-19 22:03:18 +0900

    Improve SimplifyFungibleAssetValueInputType

--- NineChronicles.Headless/GraphTypes/SimplifyFungibleAssetValueInputType.cs
+++ NineChronicles.Headless/GraphTypes/SimplifyFungibleAssetValueInputType.cs
@@ -1,31 +1,54 @@
 using System.Collections.Generic;
-using System.Numerics;
+using GraphQL;
 using GraphQL.Types;
-using Libplanet.Assets;
 
 namespace NineChronicles.Headless.GraphTypes
 {
-    public class SimplifyFungibleAssetValueInputType : InputObjectGraphType<FungibleAssetValue>
+    public class SimplifyFungibleAssetValueInputType :
+        InputObjectGraphType<(string currencyTicker, string value)>
     {
         public SimplifyFungibleAssetValueInputType()
         {
             Name = "SimplifyFungibleAssetValueInput";
-            Field<NonNullGraphType<SimplifyCurrencyInputType>>(
-                name: "currency");
-            Field<NonNullGraphType<BigIntGraphType>>(
-                name: "majorUnit",
-                description: "Major unit of currency quantity.");
-            Field<NonNullGraphType<BigIntGraphType>>(
-                name: "minorUnit",
-                description: "Minor unit of currency quantity.");
+            Description = "A fungible asset value ticker and amount." +
+                          "You can specify either currencyEnum or currencyTicker.";
+
+            Field<CurrencyEnumType>(
+                name: "currencyEnum",
+                description: "A currency type to be loaded.");
+
+            Field<StringGraphType>(
+                name: "currencyTicker",
+                description: "A currency ticker to be loaded.");
+
+            Field<NonNullGraphType<StringGraphType>>(
+                name: "value",
+                description: "A numeric string to parse.  Can consist of digits, " +
+                             "plus (+), minus (-), and decimal separator (.)." +
+                             " <see cref=\"FungibleAssetValue.Parse(Currency, string)\" />");
         }
 
         public override object ParseDictionary(IDictionary<string, object?> value)
         {
-            var currency = (Currency)value["currency"]!;
-            var majorUnit = (BigInteger)value["majorUnit"]!;
-            var minorUnit = (BigInteger)value["minorUnit"]!;
-            return new FungibleAssetValue(currency, majorUnit, minorUnit);
+            var value2 = (string)value["value"]!;
+            if (value.TryGetValue("currencyEnum", out var currencyEnum))
+            {
+                if (value.ContainsKey("currencyTicker"))
+                {
+                    throw new ExecutionError("currencyEnum and currencyTicker cannot be specified at the same time.");    
+                }
+                
+                var currencyTicker = ((CurrencyEnum)currencyEnum!).ToString();
+                return (currencyTicker, value: value2);
+            }
+            else if (value.TryGetValue("currencyTicker", out var currencyTicker))
+            {
+                return ((string)currencyTicker!, value: value2);
+            }
+            else
+            {
+                throw new ExecutionError("currencyEnum or currencyTicker must be specified.");
+            }
         }
     }
 }
