82a047e33b119ed1c9b81e3c01bc01c53b17847a
Hyun Seungmin <boscohyun@gmail.com>
2023-06-19 21:57:36 +0900

    Implement GetCurrency and GetFungibleAssetValue methods to StandaloneContext

--- NineChronicles.Headless/StandaloneContext.cs
+++ NineChronicles.Headless/StandaloneContext.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Concurrent;
+using System.Numerics;
 using System.Reactive.Subjects;
+using Bencodex.Types;
+using Lib9c;
 using Libplanet;
 using Libplanet.Assets;
 using Libplanet.Blockchain;
@@ -8,6 +11,7 @@ using Libplanet.KeyStore;
 using Libplanet.Net;
 using Libplanet.Headless;
 using Libplanet.Store;
+using Nekoyume;
 using Nekoyume.Model.State;
 using NineChronicles.Headless.GraphTypes;
 
@@ -45,6 +49,8 @@ namespace NineChronicles.Headless
 
         public Swarm? Swarm { get; internal set; }
 
+        public Currency? NCG { get; internal set; }
+
         internal TimeSpan DifferentAppProtocolVersionEncounterInterval { get; set; } = TimeSpan.FromSeconds(30);
 
         internal TimeSpan NotificationInterval { get; set; } = TimeSpan.FromSeconds(30);
@@ -54,5 +60,91 @@ namespace NineChronicles.Headless
         internal TimeSpan MonsterCollectionStateInterval { get; set; } = TimeSpan.FromSeconds(30);
 
         internal TimeSpan MonsterCollectionStatusInterval { get; set; } = TimeSpan.FromSeconds(30);
+
+        public bool TryGetCurrency(CurrencyEnum currencyEnum, out Currency? currency)
+        {
+            return TryGetCurrency(currencyEnum.ToString(), out currency);
+        }
+
+        public bool TryGetCurrency(string ticker, out Currency? currency)
+        {
+            currency = ticker switch
+            {
+                "NCG" => GetNCG(),
+                _ => Currencies.GetMinterlessCurrency(ticker),
+            };
+            return currency is not null;
+        }
+
+        private Currency? GetNCG()
+        {
+            if (NCG is not null)
+            {
+                return NCG;
+            }
+
+            if (BlockChain?.GetState(Addresses.GoldCurrency) is
+                Dictionary goldCurrencyDict)
+            {
+                var goldCurrency = new GoldCurrencyState(goldCurrencyDict);
+                NCG = goldCurrency.Currency;
+            }
+
+            return NCG;
+        }
+
+        public bool TryGetFungibleAssetValue(
+            CurrencyEnum currencyEnum,
+            BigInteger majorUnit,
+            BigInteger minorUnit,
+            out FungibleAssetValue? fungibleAssetValue)
+        {
+            return TryGetFungibleAssetValue(
+                currencyEnum.ToString(),
+                majorUnit,
+                minorUnit,
+                out fungibleAssetValue);
+        }
+
+        public bool TryGetFungibleAssetValue(
+            string ticker,
+            BigInteger majorUnit,
+            BigInteger minorUnit,
+            out FungibleAssetValue? fungibleAssetValue)
+        {
+            if (TryGetCurrency(ticker, out var currency))
+            {
+                fungibleAssetValue = new FungibleAssetValue(currency!.Value, majorUnit, minorUnit);
+                return true;
+            }
+
+            fungibleAssetValue = null;
+            return false;
+        }
+
+        public bool TryGetFungibleAssetValue(
+            CurrencyEnum currencyEnum,
+            string value,
+            out FungibleAssetValue? fungibleAssetValue)
+        {
+            return TryGetFungibleAssetValue(
+                currencyEnum.ToString(),
+                value,
+                out fungibleAssetValue);
+        }
+
+        public bool TryGetFungibleAssetValue(
+            string ticker,
+            string value,
+            out FungibleAssetValue? fungibleAssetValue)
+        {
+            if (TryGetCurrency(ticker, out var currency))
+            {
+                fungibleAssetValue = FungibleAssetValue.Parse(currency!.Value, value);
+            }
+
+            fungibleAssetValue = null;
+            return false;
+        }
     }
 }
