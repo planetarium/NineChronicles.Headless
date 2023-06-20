8a9715f736612bdbdeea7cee1dea6140bacf9095
Yang Chun Ung <qooraven@gmail.com>
2023-06-20 15:00:54 +0900

    Update NineChronicles.Headless/GraphTypes/States/Models/MeadPledgeType.cs
    Co-authored-by: Lee Dogeon <dev.moreal@gmail.com>

--- NineChronicles.Headless/GraphTypes/States/Models/MeadPledgeType.cs
+++ NineChronicles.Headless/GraphTypes/States/Models/MeadPledgeType.cs
@@ -8,8 +8,8 @@ public class MeadPledgeType : ObjectGraphType<(Address? Address, bool Approved, 
 {
     public MeadPledgeType()
     {
-        Field<AddressType>(name: "patronAddress", resolve: context => context.Source.Item1);
-        Field<NonNullGraphType<BooleanGraphType>>(name: "approved", resolve: context => context.Source.Item2);
-        Field<NonNullGraphType<IntGraphType>>(name: "mead", resolve: context => context.Source.Item3);
+        Field<AddressType>(name: "patronAddress", resolve: context => context.Source.Address);
+        Field<NonNullGraphType<BooleanGraphType>>(name: "approved", resolve: context => context.Source.Approved);
+        Field<NonNullGraphType<IntGraphType>>(name: "mead", resolve: context => context.Source.Mead);
     }
 }
