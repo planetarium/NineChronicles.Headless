ad35c85d273004a54eff7d4e8f7dbc15f5d01209
Hyun Seungmin <boscohyun@gmail.com>
2023-07-03 18:36:08 +0900

    Apply `SimplifyCurrencyInputType` into `SimplifyFungibleAssetValueInputType`

--- NineChronicles.Headless/GraphTypes/SimplifyFungibleAssetValueInputType.cs
+++ NineChronicles.Headless/GraphTypes/SimplifyFungibleAssetValueInputType.cs
@@ -2,53 +2,29 @@ using System.Collections.Generic;
 using GraphQL;
 using GraphQL.Types;
 
-namespace NineChronicles.Headless.GraphTypes
+namespace NineChronicles.Headless.GraphTypes;
+
+public class SimplifyFungibleAssetValueInputType :
+    InputObjectGraphType<(string currencyTicker, string value)>
 {
-    public class SimplifyFungibleAssetValueInputType :
-        InputObjectGraphType<(string currencyTicker, string value)>
+    public SimplifyFungibleAssetValueInputType()
     {
-        public SimplifyFungibleAssetValueInputType()
-        {
-            Name = "SimplifyFungibleAssetValueInput";
-            Description = "A fungible asset value ticker and amount." +
-                          "You can specify either currencyEnum or currencyTicker.";
+        Name = "SimplifyFungibleAssetValueInput";
+        Description = "A fungible asset value ticker and amount." +
+                      "You can specify either currencyEnum or currencyTicker.";
 
-            Field<CurrencyEnumType>(
-                name: "currencyEnum",
-                description: "A currency type to be loaded.");
+        SimplifyCurrencyInputType.SetFields(this);
+        Field<NonNullGraphType<StringGraphType>>(
+            name: "value",
+            description: "A numeric string to parse.  Can consist of digits, " +
+                         "plus (+), minus (-), and decimal separator (.)." +
+                         " <see cref=\"FungibleAssetValue.Parse(Currency, string)\" />");
+    }
 
-            Field<StringGraphType>(
-                name: "currencyTicker",
-                description: "A currency ticker to be loaded.");
-
-            Field<NonNullGraphType<StringGraphType>>(
-                name: "value",
-                description: "A numeric string to parse.  Can consist of digits, " +
-                             "plus (+), minus (-), and decimal separator (.)." +
-                             " <see cref=\"FungibleAssetValue.Parse(Currency, string)\" />");
-        }
-
-        public override object ParseDictionary(IDictionary<string, object?> value)
-        {
-            var value2 = (string)value["value"]!;
-            if (value.TryGetValue("currencyEnum", out var currencyEnum))
-            {
-                if (value.ContainsKey("currencyTicker"))
-                {
-                    throw new ExecutionError("currencyEnum and currencyTicker cannot be specified at the same time.");
-                }
-
-                var currencyTicker = ((CurrencyEnum)currencyEnum!).ToString();
-                return (currencyTicker, value: value2);
-            }
-            else if (value.TryGetValue("currencyTicker", out var currencyTicker))
-            {
-                return ((string)currencyTicker!, value: value2);
-            }
-            else
-            {
-                throw new ExecutionError("currencyEnum or currencyTicker must be specified.");
-            }
-        }
+    public override object ParseDictionary(IDictionary<string, object?> value)
+    {
+        var value2 = (string)value["value"]!;
+        var currencyTicker = SimplifyCurrencyInputType.Parse(value);
+        return (currencyTicker, value: value2);
     }
 }
