0848c2e7b6918e890749e5d49df614fcd7b6f57b
Hyun Seungmin <boscohyun@gmail.com>
2023-07-04 14:00:50 +0900

    Introduce `FungibleAssetValueFactory` util

---
+++ NineChronicles.Headless/Utils/FungibleAssetValueFactory.cs
@@ -0,0 +1,70 @@
+using System.Numerics;
+using Libplanet.Assets;
+using NineChronicles.Headless.GraphTypes;
+
+namespace NineChronicles.Headless.Utils;
+
+public class FungibleAssetValueFactory
+{
+    private readonly CurrencyFactory _currencyFactory;
+
+    public FungibleAssetValueFactory(CurrencyFactory currencyFactory)
+    {
+        _currencyFactory = currencyFactory;
+    }
+
+    public bool TryGetFungibleAssetValue(
+        CurrencyEnum currencyEnum,
+        BigInteger majorUnit,
+        BigInteger minorUnit,
+        out FungibleAssetValue fungibleAssetValue)
+    {
+        return TryGetFungibleAssetValue(
+            currencyEnum.ToString(),
+            majorUnit,
+            minorUnit,
+            out fungibleAssetValue);
+    }
+
+    public bool TryGetFungibleAssetValue(
+        string ticker,
+        BigInteger majorUnit,
+        BigInteger minorUnit,
+        out FungibleAssetValue fungibleAssetValue)
+    {
+        if (!_currencyFactory.TryGetCurrency(ticker, out var currency))
+        {
+            fungibleAssetValue = default;
+            return false;
+        }
+
+        fungibleAssetValue = new FungibleAssetValue(currency, majorUnit, minorUnit);
+        return true;
+    }
+
+    public bool TryGetFungibleAssetValue(
+        CurrencyEnum currencyEnum,
+        string value,
+        out FungibleAssetValue fungibleAssetValue)
+    {
+        return TryGetFungibleAssetValue(
+            currencyEnum.ToString(),
+            value,
+            out fungibleAssetValue);
+    }
+
+    public bool TryGetFungibleAssetValue(
+        string ticker,
+        string value,
+        out FungibleAssetValue fungibleAssetValue)
+    {
+        if (!_currencyFactory.TryGetCurrency(ticker, out var currency))
+        {
+            fungibleAssetValue = default;
+            return false;
+        }
+
+        fungibleAssetValue = FungibleAssetValue.Parse(currency, value);
+        return true;
+    }
+}
