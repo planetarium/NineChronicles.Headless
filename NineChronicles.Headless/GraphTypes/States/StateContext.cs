383b1cad7d08df932708188b97b4534615e35a95
Hyun Seungmin <boscohyun@gmail.com>
2023-07-04 14:02:34 +0900

    Expand `StateContext`

--- NineChronicles.Headless/GraphTypes/States/StateContext.cs
+++ NineChronicles.Headless/GraphTypes/States/StateContext.cs
@@ -1,24 +1,35 @@
+#nullable enable
+
 using System.Collections.Generic;
 using Bencodex.Types;
 using Libplanet;
-using Libplanet.Action;
 using Libplanet.Assets;
 using Libplanet.State;
+using NineChronicles.Headless.Utils;
 
 namespace NineChronicles.Headless.GraphTypes.States
 {
     public class StateContext
     {
-        public StateContext(AccountStateGetter accountStateGetter, AccountBalanceGetter accountBalanceGetter, long blockIndex)
+        public StateContext(
+            AccountStateGetter accountStateGetter,
+            AccountBalanceGetter accountBalanceGetter,
+            long blockIndex)
         {
             AccountStateGetter = accountStateGetter;
             AccountBalanceGetter = accountBalanceGetter;
             BlockIndex = blockIndex;
+            CurrencyFactory = new CurrencyFactory(accountStateGetter);
+            FungibleAssetValueFactory = new FungibleAssetValueFactory(CurrencyFactory);
         }
 
         public AccountStateGetter AccountStateGetter { get; }
         public AccountBalanceGetter AccountBalanceGetter { get; }
         public long BlockIndex { get; }
+
+        public CurrencyFactory CurrencyFactory { get; }
+
+        public FungibleAssetValueFactory FungibleAssetValueFactory { get; }
 
         public IValue? GetState(Address address) =>
             AccountStateGetter(new[] { address })[0];
