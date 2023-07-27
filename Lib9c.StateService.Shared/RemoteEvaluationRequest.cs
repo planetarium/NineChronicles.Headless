7e3c45767e26732282b440f275f9013d4d8a4871
moreal <dev.moreal@gmail.com>
2023-07-27 16:14:47 +0900

    chore: move lib9c-stateservice

--- Lib9c.StateService.Shared/RemoteEvaluationRequest.cs
+++
@@ -1,6 +0,0 @@
-namespace Lib9c.StateService.Shared;
-
-public class RemoteEvaluationRequest
-{
-    public byte[] PreEvaluationBlock { get; set; }
-}
--- Lib9c.StateService.Shared/RemoteEvaluationResponse.cs
+++
@@ -1,6 +0,0 @@
-namespace Lib9c.StateService.Shared;
-
-public class RemoteEvaluationResponse
-{
-    public byte[][] Evaluations { get; set; }
-}
