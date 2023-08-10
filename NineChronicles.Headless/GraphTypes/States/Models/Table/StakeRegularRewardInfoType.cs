30a07fad2441bf385cf4011fb8c33f16f308413f
Hyun Seungmin <boscohyun@gmail.com>
2023-08-10 21:46:39 +0900

    feat(graph-type): apply to StakeRegularRewardInfoType

--- NineChronicles.Headless/GraphTypes/States/Models/Table/StakeRegularRewardInfoType.cs
+++ NineChronicles.Headless/GraphTypes/States/Models/Table/StakeRegularRewardInfoType.cs
@@ -11,19 +11,28 @@ namespace NineChronicles.Headless.GraphTypes.States.Models.Table
                 nameof(StakeRegularRewardSheet.RewardInfo.ItemId),
                 resolve: context => context.Source.ItemId
             );
+#pragma warning disable CS0618
             Field<NonNullGraphType<IntGraphType>>(
                 nameof(StakeRegularRewardSheet.RewardInfo.Rate),
                 resolve: context => context.Source.Rate
             );
+#pragma warning restore CS0618
             Field<NonNullGraphType<StakeRewardEnumType>>(
                 nameof(StakeRegularRewardSheet.RewardInfo.Type),
                 resolve: context => context.Source.Type
             );
-            Field<StakeRewardEnumType>(
+            Field<StringGraphType>(
                 nameof(StakeRegularRewardSheet.RewardInfo.CurrencyTicker),
                 resolve: context => context.Source.CurrencyTicker
             );
-
+            Field<IntGraphType>(
+                nameof(StakeRegularRewardSheet.RewardInfo.CurrencyDecimalPlaces),
+                resolve: context => context.Source.CurrencyDecimalPlaces
+            );
+            Field<DecimalGraphType>(
+                nameof(StakeRegularRewardSheet.RewardInfo.DecimalRate),
+                resolve: context => context.Source.DecimalRate
+            );
         }
     }
 }
