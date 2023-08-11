fb8dc9e225e1c852f4b899ea7a0097a3d12ccbe2
Lee Dogeon <dev.moreal@gmail.com>
2023-08-11 15:11:02 +0900

    Merge pull request #2190 from planetarium/feature/graphql/equipment-filter

--- NineChronicles.Headless/GraphTypes/States/Models/Item/InventoryType.cs
+++ NineChronicles.Headless/GraphTypes/States/Models/Item/InventoryType.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using GraphQL;
 using GraphQL.Types;
 using Nekoyume.Model.Item;
+using NineChronicles.Headless.GraphTypes.States.Models.Item.Enum;
 
 namespace NineChronicles.Headless.GraphTypes.States.Models.Item
 {
@@ -20,7 +22,50 @@ namespace NineChronicles.Headless.GraphTypes.States.Models.Item
             );
             Field<NonNullGraphType<ListGraphType<NonNullGraphType<EquipmentType>>>>(
                 nameof(Inventory.Equipments),
-                description: "List of Equipments."
+                description: "List of Equipments.",
+                arguments: new QueryArguments(
+                    new QueryArgument<BooleanGraphType>
+                    {
+                        Name = "equipped",
+                        Description = "filter equipped inventory item"
+                    },
+                    new QueryArgument<ItemSubTypeEnumType>
+                    {
+                        Name = "itemSubType",
+                        Description = "An item subtype for fetching only equipment where " +
+                                      "its subtype is the same. If it wasn't given, you'll " +
+                                      "get all equipment without relationship to the subtype."
+                    },
+                    new QueryArgument<ListGraphType<NonNullGraphType<GuidGraphType>>>
+                    {
+                        Name = "itemIds",
+                        Description = "ItemIds for fetching only equipment where id is in " +
+                                      "the given argument."
+                    }),
+                resolve: context =>
+                {
+                    var equipments = context.Source.Equipments;
+                    var equippedFilter = context.GetArgument<bool?>("equipped");
+                    var itemSubTypeFilter = context.GetArgument<ItemSubType?>("itemSubType");
+                    var itemIdsFilter = context.GetArgument<Guid[]?>("itemIds");
+                    if (equippedFilter.HasValue)
+                    {
+                        equipments = equipments.Where(x => x.equipped == equippedFilter.Value).ToList();
+                    }
+
+                    if (itemSubTypeFilter is not null)
+                    {
+                        equipments = equipments.Where(equipment => equipment.ItemSubType == itemSubTypeFilter);
+                    }
+
+                    if (itemIdsFilter is not null)
+                    {
+                        var set = itemIdsFilter.ToHashSet();
+                        equipments = equipments.Where(equipment => set.Contains(equipment.ItemId));
+                    }
+
+                    return equipments;
+                }
             );
             Field<NonNullGraphType<ListGraphType<NonNullGraphType<CostumeType>>>>(
                 nameof(Inventory.Costumes),
