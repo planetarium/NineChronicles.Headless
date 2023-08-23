7b29def23a6e5b63a5c4896d5672a5ab7992cacf
area363 <kidonseo@gmail.com>
2023-08-23 13:29:38 +0900

    update interval

--- NineChronicles.Headless/Middleware/HttpCaptureMiddleware.cs
+++ NineChronicles.Headless/Middleware/HttpCaptureMiddleware.cs
@@ -15,7 +15,7 @@ namespace NineChronicles.Headless.Middleware
     public class HttpCaptureMiddleware
     {
         private const int MultiAccountManagementTime = 5;
-        private const int MultiAccountTxInterval = 5;
+        private const int MultiAccountTxInterval = 10;
         private static Dictionary<Address, DateTimeOffset> _multiAccountTxIntervalTracker = new();
         private static Dictionary<Address, DateTimeOffset> _multiAccountList = new();
         private readonly RequestDelegate _next;
