a7563f35181dc1fc5564948d6c81b12d9738c53e
area363 <kidonseo@gmail.com>
2023-09-01 12:59:22 +0900

    update management time

--- NineChronicles.Headless/Middleware/GrpcCaptureMiddleware.cs
+++ NineChronicles.Headless/Middleware/GrpcCaptureMiddleware.cs
@@ -14,7 +14,7 @@ namespace NineChronicles.Headless.Middleware
 {
     public class GrpcCaptureMiddleware : Interceptor
     {
-        private const int MultiAccountManagementTime = 5;
+        private const int MultiAccountManagementTime = 10;
         private const int MultiAccountTxInterval = 10;
         private static Dictionary<Address, DateTimeOffset> _multiAccountTxIntervalTracker = new();
         private static Dictionary<Address, DateTimeOffset> _multiAccountList = new();
