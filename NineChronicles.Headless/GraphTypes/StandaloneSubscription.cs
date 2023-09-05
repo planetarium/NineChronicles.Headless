28f36d2b93cfb56230702ef868477df450fd702f
Say Cheong <greymistcube@gmail.com>
2023-09-05 14:56:57 +0900

    Accommodate API changes

--- NineChronicles.Headless/GraphTypes/StandaloneSubscription.cs
+++ NineChronicles.Headless/GraphTypes/StandaloneSubscription.cs
@@ -329,21 +329,17 @@ namespace NineChronicles.Headless.GraphTypes
                     txExecutedBlock.Index,
                     txExecutedBlock.Hash.ToString(),
                     null,
-                    null,
                     success.UpdatedStates
                         .Select(kv => new KeyValuePair<Address, IValue>(
                             kv.Key,
                             kv.Value))
                         .ToImmutableDictionary(),
-                    success.FungibleAssetsDelta,
                     success.UpdatedFungibleAssets),
                 TxFailure failure => new TxResult(
                     TxStatus.FAILURE,
                     txExecutedBlock.Index,
                     txExecutedBlock.Hash.ToString(),
                     failure.ExceptionName,
-                    failure.ExceptionMetadata,
-                    null,
                     null,
                     null),
                 _ => null
