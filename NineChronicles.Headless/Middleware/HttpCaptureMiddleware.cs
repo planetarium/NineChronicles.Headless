c0846829a23bbde98e265deede5d91283b0e3e73
area363 <kidonseo@gmail.com>
2023-10-04 14:56:17 +0900

    update middleware

--- NineChronicles.Headless/Middleware/HttpCaptureMiddleware.cs
+++ NineChronicles.Headless/Middleware/HttpCaptureMiddleware.cs
@@ -1,7 +1,12 @@
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Libplanet.Common;
+using Libplanet.Types.Tx;
 using Microsoft.AspNetCore.Http;
 using Serilog;
+using Exception = System.Exception;
 using ILogger = Serilog.ILogger;
 
 namespace NineChronicles.Headless.Middleware
@@ -28,6 +33,26 @@ namespace NineChronicles.Headless.Middleware
                 _logger.Information("[GRAPHQL-REQUEST-CAPTURE] IP: {IP} Method: {Method} Endpoint: {Path} {Body}",
                     remoteIp, context.Request.Method, context.Request.Path, body);
                 context.Request.Body.Seek(0, SeekOrigin.Begin);
+                if (body.Contains("stageTransaction"))
+                {
+                    try
+                    {
+                        var pattern = "64313.*6565";
+                        var txPayload = Regex.Match(body, pattern).ToString();
+                        byte[] bytes = ByteUtil.ParseHex(txPayload);
+                        Transaction tx = Transaction.Deserialize(bytes);
+                        var agent = tx.Signer;
+                        _logger.Information("[GRAPHQL-REQUEST-CAPTURE] IP: {IP} Agent: {Agent} Tx: {Path}",
+                            remoteIp, agent, tx.Actions.Actions.FirstOrDefault());
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Error(
+                            "[GRAPHQL-REQUEST-CAPTURE] Error message: {message} Stacktrace: {stackTrace}",
+                            ex.Message,
+                            ex.StackTrace);
+                    }
+                }
             }
 
             await _next(context);
