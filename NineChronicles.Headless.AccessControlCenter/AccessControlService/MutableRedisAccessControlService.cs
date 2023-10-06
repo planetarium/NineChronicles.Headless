2a12f404754e58acc98cac32fc98a94b600a6820
Atralupus <me@atralupus.com>
2023-10-06 17:06:30 +0900

    feat: Introduce AccessControlCenter project
    Port settings
    
    fix lint
    
    Add Dockerfiles
    
    Remove arguments

---
+++ NineChronicles.Headless.AccessControlCenter/AccessControlService/MutableRedisAccessControlService.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using Libplanet.Crypto;
+using NineChronicles.Headless.Services;
+
+namespace NineChronicles.Headless.AccessControlCenter.AccessControlService
+{
+    public class MutableRedisAccessControlService : RedisAccessControlService, IMutableAccessControlService
+    {
+
+        public MutableRedisAccessControlService(string storageUri) : base(storageUri)
+        {
+        }
+
+        public void DenyAccess(Address address)
+        {
+            _db.StringSet(address.ToString(), "denied");
+        }
+
+        public void AllowAccess(Address address)
+        {
+            _db.KeyDelete(address.ToString());
+        }
+
+        public List<Address> ListBlockedAddresses(int offset, int limit)
+        {
+            var server = _db.Multiplexer.GetServer(_db.Multiplexer.GetEndPoints().First());
+            var keys = server
+                .Keys()
+                .Select(k => new Address(k.ToString()))
+                .ToList();
+
+            return keys.Skip(offset).Take(limit).ToList();
+        }
+    }
+}
---
+++ NineChronicles.Headless.AccessControlCenter/AccessControlService/MutableSqliteAccessControlService.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using Libplanet.Crypto;
+using NineChronicles.Headless.Services;
+
+namespace NineChronicles.Headless.AccessControlCenter.AccessControlService
+{
+    public class MutableSqliteAccessControlService : SQLiteAccessControlService, IMutableAccessControlService
+    {
+        private const string DenyAccessSql =
+            "INSERT OR IGNORE INTO blocklist (address) VALUES (@Address)";
+        private const string AllowAccessSql = "DELETE FROM blocklist WHERE address=@Address";
+
+        public MutableSqliteAccessControlService(string connectionString) : base(connectionString)
+        {
+        }
+
+        public void DenyAccess(Address address)
+        {
+            using var connection = new SqliteConnection(_connectionString);
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = DenyAccessSql;
+            command.Parameters.AddWithValue("@Address", address.ToString());
+            command.ExecuteNonQuery();
+        }
+
+        public void AllowAccess(Address address)
+        {
+            using var connection = new SqliteConnection(_connectionString);
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = AllowAccessSql;
+            command.Parameters.AddWithValue("@Address", address.ToString());
+            command.ExecuteNonQuery();
+        }
+
+        public List<Address> ListBlockedAddresses(int offset, int limit)
+        {
+            var blockedAddresses = new List<Address>();
+
+            using var connection = new SqliteConnection(_connectionString);
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = $"SELECT address FROM blocklist LIMIT @Limit OFFSET @Offset";
+            command.Parameters.AddWithValue("@Limit", limit);
+            command.Parameters.AddWithValue("@Offset", offset);
+
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                blockedAddresses.Add(new Address(reader.GetString(0)));
+            }
+
+            return blockedAddresses;
+        }
+    }
+}
