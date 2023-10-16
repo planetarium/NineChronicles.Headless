47e440222ba59fe5b1acf2be9c7520604113b441
Atralupus <me@atralupus.com>
2023-10-16 19:12:53 +0900

    Configuration DI

--- NineChronicles.Headless.AccessControlCenter/AcsService.cs
+++ NineChronicles.Headless.AccessControlCenter/AcsService.cs
@@ -11,27 +11,18 @@ namespace NineChronicles.Headless.AccessControlCenter
 {
     public class AcsService
     {
-        private readonly string _acsType;
-        private readonly string _acsConnectionString;
-
-        public AcsService(string acsType, string acsConnectionString)
+        public AcsService(Configuration configuration)
         {
-            _acsType = acsType;
-            _acsConnectionString = acsConnectionString;
+            Configuration = configuration;
         }
+
+        public Configuration Configuration { get; }
 
         public IHostBuilder Configure(IHostBuilder hostBuilder, int port)
         {
             return hostBuilder.ConfigureWebHostDefaults(builder =>
             {
-                builder.UseStartup(
-                    x =>
-                        new RestApiStartup(
-                            x.Configuration,
-                            _acsType,
-                            _acsConnectionString
-                        )
-                );
+                builder.UseStartup(x => new RestApiStartup(Configuration));
                 builder.ConfigureKestrel(options =>
                 {
                     options.ListenAnyIP(
@@ -47,29 +38,23 @@ namespace NineChronicles.Headless.AccessControlCenter
 
         internal class RestApiStartup
         {
-            private readonly string _acsType;
-            private readonly string _acsConnectionString;
-
-            public RestApiStartup(
-                IConfiguration configuration,
-                string acsType,
-                string acsConnectionString
-            )
+            public RestApiStartup(Configuration configuration)
             {
                 Configuration = configuration;
-                _acsType = acsType;
-                _acsConnectionString = acsConnectionString;
             }
 
-            public IConfiguration Configuration { get; }
+            public Configuration Configuration { get; }
 
             public void ConfigureServices(IServiceCollection services)
             {
                 services.AddControllers();
 
                 var accessControlService = MutableAccessControlServiceFactory.Create(
-                    Enum.Parse<MutableAccessControlServiceFactory.StorageType>(_acsType, true),
-                    _acsConnectionString
+                    Enum.Parse<MutableAccessControlServiceFactory.StorageType>(
+                        Configuration.AccessControlServiceType,
+                        true
+                    ),
+                    Configuration.AccessControlServiceConnectionString
                 );
 
                 services.AddSingleton(accessControlService);
--- NineChronicles.Headless.AccessControlCenter/Program.cs
+++ NineChronicles.Headless.AccessControlCenter/Program.cs
@@ -10,20 +10,17 @@ namespace NineChronicles.Headless.AccessControlCenter
         {
             // Get configuration
             string configPath =
-                Environment.GetEnvironmentVariable("ACS_CONFIG_FILE") ?? "appsettings.json";
+                Environment.GetEnvironmentVariable("ACC_CONFIG_FILE") ?? "appsettings.json";
 
             var configurationBuilder = new ConfigurationBuilder()
                 .AddJsonFile(configPath)
-                .AddEnvironmentVariables("ACS_");
+                .AddEnvironmentVariables("ACC_");
             IConfiguration config = configurationBuilder.Build();
 
             var acsConfig = new Configuration();
             config.Bind(acsConfig);
 
-            var service = new AcsService(
-                acsConfig.AccessControlServiceType,
-                acsConfig.AccessControlServiceConnectionString
-            );
+            var service = new AcsService(acsConfig);
             var hostBuilder = service.Configure(Host.CreateDefaultBuilder(), acsConfig.Port);
             var host = hostBuilder.Build();
             host.Run();
