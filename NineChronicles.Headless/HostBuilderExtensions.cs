496f209ef89e3a0ecc2454c103a944f2da29ef2c
Yang Chun Ung <qooraven@gmail.com>
2023-11-18 22:43:45 +0900

    Increase size

--- NineChronicles.Headless/HostBuilderExtensions.cs
+++ NineChronicles.Headless/HostBuilderExtensions.cs
@@ -104,8 +104,8 @@ namespace NineChronicles.Headless
                 {
                     hostBuilder.ConfigureKestrel(options =>
                     {
-                        options.Limits.Http2.InitialConnectionWindowSize = 1024 * 1024 * 4;
-                        options.Limits.Http2.InitialStreamWindowSize = 1024 * 1024 * 4;
+                        options.Limits.Http2.InitialConnectionWindowSize = 1073741824;
+                        options.Limits.Http2.InitialStreamWindowSize = 1073741824;
                         options.ListenAnyIP(properties.RpcListenPort, listenOptions =>
                         {
                             listenOptions.Protocols = HttpProtocols.Http2;
