2cd4033aa6a1057e2875b51027556fc859344a63
Robert <ghdwlgud4545@gmail.com>
2023-12-02 13:35:36 +0900

    Refactor Jwt authentication

--- NineChronicles.Headless/Middleware/JwtAuthenticationMiddleware.cs
+++ NineChronicles.Headless/Middleware/JwtAuthenticationMiddleware.cs
@@ -1,22 +1,26 @@
 ﻿using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
 using Microsoft.IdentityModel.Tokens;
+using Serilog;
 
 namespace NineChronicles.Headless.Middleware;
 
 public class JwtAuthenticationMiddleware : IMiddleware
 {
-    
+    private readonly ILogger _logger;
     private readonly JwtSecurityTokenHandler _tokenHandler = new JwtSecurityTokenHandler();
     private readonly TokenValidationParameters _validationParams;
 
-    public JwtAuthenticationMiddleware(IConfiguration configuration)
+    public JwtAuthenticationMiddleware(IConfiguration configuration, ILogger logger)
     {
+        _logger = logger;
         var jwtConfig = configuration.GetSection("Jwt");
         var issuer = jwtConfig["Issuer"] ?? "";
         var key = jwtConfig["Key"] ?? "";
@@ -30,7 +34,7 @@ public class JwtAuthenticationMiddleware : IMiddleware
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key.PadRight(512/8, '\0')))
         };
     }
-    
+
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         context.Request.Headers.TryGetValue("Authorization", out var authorization);
@@ -38,30 +42,36 @@ public class JwtAuthenticationMiddleware : IMiddleware
         {
             try
             {
-                string[] headerValues = authorization[0].Split(" ");
-                string scheme = headerValues[0];
+                var (scheme, token) = ExtractSchemeAndToken(authorization);
                 if (scheme == "Bearer")
                 {
-                    string token = headerValues[1];
-
-                    _tokenHandler.ValidateToken(token, _validationParams, out SecurityToken validatedToken);
-                    var jwt = (JwtSecurityToken)validatedToken;
-                    foreach (var claim in jwt.Claims)
-                    {
-                        context.User.AddIdentity(
-                            new ClaimsIdentity(
-                                new[]
-                                {
-                                    new Claim(claim.Type, claim.Value)
-                                }));
-                    }
+                    ValidateTokenAndAddClaims(context, token);
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Authorization error {e.Message}");
+                _logger.Error($"Authorization error {e.Message}");
             }
         }
         await next(context);
+    }
+
+    private (string scheme, string token) ExtractSchemeAndToken(StringValues authorizationHeader)
+    {
+        var headerValues = authorizationHeader[0].Split(" ");
+        if (headerValues.Length < 2)
+        {
+            throw new ArgumentException("Invalid Authorization header format.");
+        }
+
+        return (headerValues[0], headerValues[1]);
+    }
+
+    private void ValidateTokenAndAddClaims(HttpContext context, string token)
+    {
+        _tokenHandler.ValidateToken(token, _validationParams, out SecurityToken validatedToken);
+        var jwt = (JwtSecurityToken)validatedToken;
+        var claims = jwt.Claims.Select(claim => new Claim(claim.Type, claim.Value));
+        context.User.AddIdentity(new ClaimsIdentity(claims));
     }
 }
--- NineChronicles.Headless/Properties/JwtOptions.cs
+++ NineChronicles.Headless/Properties/JwtOptions.cs
@@ -3,7 +3,7 @@
 public class JwtOptions
 {
     public bool EnableJwtAuthentication { get; }
-    
+
     public string Key { get; } = "";
 
     public string Issuer { get; } = "planetariumhq.com";
