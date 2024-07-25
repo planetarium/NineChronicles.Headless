15976a48429d682ba1a42a095ca1e03466a06685
Yang Chun Ung <qooraven@gmail.com>
2024-07-25 21:00:04 +0900

    Log state execute arguments for replay

--- NineChronicles.Headless.Executable/Commands/ReplayCommand.cs
+++ NineChronicles.Headless.Executable/Commands/ReplayCommand.cs
@@ -433,10 +433,16 @@ namespace NineChronicles.Headless.Executable.Commands
                 .Select(ToAction)
                 .Cast<IAction>()
                 .ToImmutableList();
+            var blockIndex = transactionResult.BlockIndex ?? 0;
+            var blockProtocolVersion = (int)protocolVersion;
+            var preEvaluationHash = HashDigest<SHA256>.FromString(preEvaluationHashValue);
+            byte[] preEvaluationHashBytes = preEvaluationHash.ToByteArray();
+            int seed = ActionEvaluator.GenerateRandomSeed(preEvaluationHashBytes, transaction.Signature, 0);
+            _console.Out.WriteLine($"BlockIndex: {blockIndex}, BlockProtocolVersion: {blockProtocolVersion}, Miner: {miner}, PreviousState: {previousStateRootHash}, RandomSeed: {seed}, Signer: {transaction.Signer}, TxId: {transactionId}");
             var actionEvaluations = EvaluateActions(
-                preEvaluationHash: HashDigest<SHA256>.FromString(preEvaluationHashValue),
-                blockIndex: transactionResult.BlockIndex ?? 0,
-                blockProtocolVersion: (int)protocolVersion,
+                preEvaluationHash: preEvaluationHash,
+                blockIndex: blockIndex,
+                blockProtocolVersion: blockProtocolVersion,
                 txid: transaction.Id,
                 previousStates: previousStates,
                 miner: miner,
