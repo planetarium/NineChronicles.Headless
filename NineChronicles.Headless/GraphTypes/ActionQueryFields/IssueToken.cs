dc8e4752d4a24b78d8a6a8df3ba441c31965d3c3
Yang Chun Ung <qooraven@gmail.com>
2024-08-05 20:01:13 +0900

    Fix typo

--- NineChronicles.Headless/GraphTypes/ActionQueryFields/IssueToken.cs
+++ NineChronicles.Headless/GraphTypes/ActionQueryFields/IssueToken.cs
@@ -35,7 +35,7 @@ namespace NineChronicles.Headless.GraphTypes
                 resolve: context =>
                 {
                     var fungibleAssetValues = context.GetArgument<List<FungibleAssetValue>>("fungibleAssetValues");
-                    var items = context.GetArgument<List<(int ItemIdAndCountInputType, int count)>>("items");
+                    var items = context.GetArgument<List<(int itemId, int count)>>("items");
                     var avatarAddress = context.GetArgument<Address>("avatarAddress");
                     ActionBase action = new IssueToken
                     {
