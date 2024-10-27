76c1e57c9d4b8e104930957359825fbcdc07efcc
강은빈 Eunbeen Kang <145640625+river20s@users.noreply.github.com>
2024-10-27 15:38:39 +0900

    Support more minterless currencies (#2612)

--- NineChronicles.Headless/GraphTypes/CurrencyEnumType.cs
+++ NineChronicles.Headless/GraphTypes/CurrencyEnumType.cs
@@ -9,7 +9,13 @@ namespace NineChronicles.Headless.GraphTypes
         CRYSTAL,
         NCG,
         GARAGE,
-        MEAD
+        MEAD,
+        RUNE_GOLDENLEAF,
+        RUNE_ADVENTURER,
+        RUNESTONE_FREYA_LIBERATION,
+        RUNESTONE_FREYA_BLESSING,
+        RUNESTONE_ODIN_WEAKNESS,
+        RUNESTONE_ODIN_WISDOM
     }
 
     public class CurrencyEnumType : EnumerationGraphType<CurrencyEnum>
