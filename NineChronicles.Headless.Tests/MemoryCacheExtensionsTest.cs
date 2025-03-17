6928ee1a2ca427568ad2d10e34b72fd4d81b8951
Yang Chun Ung <qooraven@gmail.com>
2025-03-17 23:27:41 +0900

    Add test case

--- NineChronicles.Headless.Tests/MemoryCacheExtensionsTest.cs
+++ NineChronicles.Headless.Tests/MemoryCacheExtensionsTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Bencodex;
 using Bencodex.Types;
@@ -35,5 +37,18 @@ public class MemoryCacheExtensionsTest
         Assert.Equal(csv, cache.GetSheet(cacheKey));
         await Task.Delay(100);
         Assert.False(cache.TryGetValue(cacheKey, out byte[] _));
+    }
+
+    [Fact]
+    public void NormalizedBytes()
+    {
+        var csv = "a,b,c\n1,2,3\n4,5,6\n";
+        var csv2 = "a,b,c\r\n1,2,3\r\n4,5,6\r\n   ";
+        var expectedString = "a,b,c\n1,2,3\n4,5,6";
+        var expectedBytes = Encoding.UTF8.GetBytes(expectedString);
+        Assert.Equal(expectedString, Encoding.UTF8.GetString(MemoryCacheExtensions.NormalizedBytes(csv)));
+        Assert.Equal(expectedString, Encoding.UTF8.GetString(MemoryCacheExtensions.NormalizedBytes(csv2)));
+        Assert.Equal(expectedBytes, MemoryCacheExtensions.NormalizedBytes(csv));
+        Assert.Equal(expectedBytes, MemoryCacheExtensions.NormalizedBytes(csv2));
     }
 }
